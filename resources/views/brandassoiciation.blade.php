25ad2f4147439e7ab0f787590009666e8e439d12
Aalind01 <aalind.01@gmail.com>
2019-03-28 15:02:50 +0530

    Add homepage code for mobile.

--- resources/views/brandassoiciation.blade.php
+++ resources/views/brandassoiciation.blade.php
@@ -1,7 +1,7 @@
 <div class="brand-association">
     <div class="-text">Shop your favourite brands:</div>
     <div class="row">
-        <div class="brand-logos col-md-10">
+        <div class="brand-logos col-md-10 px-0">
             <a href="#"><img class="brand-logo" src="{{ asset('/images/brand-images/C_B_G.png') }}" alt="CBG brand"></a>
             <a href="#"><img class="brand-logo" src="{{ asset('/images/brand-images/DWR_G.png') }}" alt="DWRG brand"></a>
             <a href="#"><img class="brand-logo" src="{{ asset('/images/brand-images/P1_G.png') }}" alt="P1G brand"></a>
@@ -10,7 +10,7 @@
 
             <img class="brand-logo more-soon" src="{{ asset('/images/brand-images/more-coming-soon.png') }}" alt="and more coming soon">
         </div>
-        <div class="socials col-md-2">
+        <div class="socials col-md-2 px-0">
             <div class="-text">Get in touch</div>
             <div class="-icons">
                 <i class="fab fa-facebook-f -icon"></i>
