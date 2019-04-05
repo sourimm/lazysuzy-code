54cfc9d6a6b3dade33cfe55c1662cc0d8d69838c
Ubuntu <ubuntu@ip-172-31-46-190.us-east-2.compute.internal>
2019-04-05 16:06:25 +0000

    permission

--- public/js/app.js
+++ public/js/app.js
@@ -36659,8 +36659,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/WorkspaceDrive/My work/LazyCode/lazysuzy-code/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Volumes/WorkspaceDrive/My work/LazyCode/lazysuzy-code/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/ubuntu/lazysuzy/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/ubuntu/lazysuzy/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
