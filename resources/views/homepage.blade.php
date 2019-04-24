d7fc27a059394bd49ee8103f2277f603f80355d7
Aditya <adityasaxena602@gmail.com>
2019-04-24 18:36:10 +0530

    apology commit! front page now has a ugly login form

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -29,6 +29,15 @@
             </div>
         </div>
     </div>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+     <button type="submit" class="btn btn-primary">
+       {{ __('Login') }}
+     </button>
+    </form>
 @endsection
 
 @push('scripts')
