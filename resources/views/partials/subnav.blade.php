eb1f243758e0190c311ea6fd5a799a91776000d4
kallolpratim <ksaikia615@gmail.com>
2019-04-29 00:04:23 +0530

    dynamic listing

--- resources/views/partials/subnav.blade.php
+++ resources/views/partials/subnav.blade.php
@@ -8,24 +8,9 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                <li><a href="#">Accent <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Living</a></li>
-                    <li><a href="#">Bedroom</a></li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hall</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a href="#">Bar</a></li>
-                            <li><a href="#">Seating</a></li>
-                            <li><a href="#">Table</a></li>
-                            <li><a href="#">Storage</a></li>
-                            <li><a href="#">Sets</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Bath</a></li>
-                    <li><a href="#">Outdoor</a></li>
-                    <li><a href="#">Pet</a></li>
-                    <li><a href="#">Kids</a></li>
+                    @foreach($departments as $department)
+                        <li><a href="{{route('products' , ['department_name'=>$department->name])}}">{{$department->name}}</a></li>
+                    @endforeach
                 </ul>
             </div>
         </nav>
