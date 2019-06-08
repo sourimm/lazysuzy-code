70e49d0f7d19e14d5096a24e15e3b99e65367f91
Aalind01 <aalind.01@gmail.com>
2019-06-08 19:17:07 +0530

    Add sort type handling. Also,  Fix scroll bug

--- resources/js/components/custom-selectbox.js
+++ resources/js/components/custom-selectbox.js
@@ -6,12 +6,18 @@ export default function makeSelectBox() {
     $('select').each(function () {
         var $this = $(this), numberOfOptions = $(this).children('option').length;
 
+        //Remove previously made selectbox
+        $('#selectbox-' + $this.attr('id')).remove();
+
         $this.addClass('select-hidden');
         $this.wrap('<div class="select"></div>');
-        $this.after('<div class="select-styled"></div>');
+        $this.after('<div class="select-styled" id="selectbox-' + $this.attr('id') + '"></div>');
 
         var $styledSelect = $this.next('div.select-styled');
-        $styledSelect.text($this.children('option').eq(0).text());
+        var strSelectedText = $(this).children("option:selected") ? $(this).children("option:selected").text() : $this.children('option:selected').eq(0).text()
+        var strSelectedValue = $(this).children("option:selected") ? $(this).children("option:selected").attr('value') : $this.children('option:selected').eq(0).attr('value')
+        $styledSelect.text(strSelectedText);
+        $styledSelect.attr('active', strSelectedValue);
 
         var $list = $('<ul />', {
             'class': 'select-options'
@@ -37,6 +43,10 @@ export default function makeSelectBox() {
         $listItems.click(function (e) {
             e.stopPropagation();
             $styledSelect.text($(this).text()).removeClass('active');
+            var strSelectedValue = $(this).attr('rel');
+            $styledSelect.attr('active', strSelectedValue);
+            $(document).trigger('select-value-changed');
+            
             $this.val($(this).attr('rel'));
             $list.hide();
             //console.log($this.val());
