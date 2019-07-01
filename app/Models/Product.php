bf175f7e38417bf94b2aad9eaf4265f17b5a5408
Aditya <adityasaxena602@gmail.com>
2019-07-01 14:00:31 +0530

    added variations api

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
-use Image;
 
 class Product extends Model
 {
