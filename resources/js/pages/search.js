d56937e3d29b49b3f42ad769caa08464a4d7f740
Aalind01 <aalind.01@gmail.com>
2019-09-03 18:02:12 +0530

    Add search page js and login modal popup on clicking wishlist icon for non-auth user.

---
+++ resources/js/pages/search.js
@@ -0,0 +1,54 @@
+require('ion-rangeslider');
+require('../apis/search-api');
+
+$(document).ready(function () {
+  let iItemsToShow = 3;
+
+  $("#priceRangeSlider").change(function () {
+    $("#priceInfo").find('.low').text($(this).attr('min'));
+    $("#priceInfo").find('.high').text($(this).val());
+  });
+
+  $priceRangeSlider = $("#priceRangeSlider");
+
+  $priceRangeSlider.ionRangeSlider({
+    skin: "sharp",
+    type: "double",
+    min: 100,
+    max: 5000,
+    from: 500,
+    to: 2500,
+    prefix: "$",
+    prettify_separator: ","
+  });
+
+  $('#filterToggleBtn').click(function () {
+    $('#filters').toggleClass('show');
+  });
+
+  $('#viewItemsBtn').click(function () {
+    iItemsToShow = (iItemsToShow == 1) ? 3 : iItemsToShow - 1;
+    $('#productsContainerDiv').find('.ls-product-div').each(function () {
+      $(this).removeClass(function (index, className) {
+        return (className.match(/(^|\s)item-\S+/g) || []).join(' ');
+      });
+      $(this).addClass('item-' + iItemsToShow);
+    })
+  });
+
+  $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+      $('.dropdown-submenu .show').removeClass("show");
+    });
+
+    return false;
+  });
+
+})
---
+++ resources/views/pages/search.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.layout', ['body_class' => 'listing-main-div'])
+
+@section('middle_content')
+    <div class="listing">
+        @include('./partials/subnav')
+        <div class="listing-container main-container container">
+            <div class="d-none d-md-block">
+                {{ Breadcrumbs::render('products') }}
+            </div>
+            <div class="row">
+                <div class="filters col-md-2 d-md-block" id="filters">
+                    <div class="filter">
+                        <hr>
+                        <span class="filter-header">Brands</span>
+                        <label for="" class="clear-filter float-right">Clear</label>
+                        <ul>
+                            <li>
+                                <label class="container">CB2
+                                    <input type="checkbox" checked="checked" value="cb2">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Pier
+                                    <input type="checkbox" value="pier1">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Pottery Barn
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                        </ul>
+                        <hr>
+                    </div>
+                    <div class="filter">
+                        <span class="filter-header">Price</span>
+                        <label for="" class="clear-filter float-right">Clear</label>
+                        <input type="text" class="price-range-slider" id="priceRangeSlider" name="price_range" value="" />
+                        <hr>
+                    </div>
+
+                    <div class="filter">
+                        <span class="filter-header">Type</span>
+                        <label for="" class="clear-filter float-right">Clear</label>
+                        <ul>
+                            <li>
+                                <label class="container">Armchair
+                                    <input type="checkbox" checked="checked">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Armless
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Recliner
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Ottoman
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </li>
+                        </ul>
+                        <hr>
+                    </div>
+                    <a class="btn clearall-filter-btn" href="https://lazysuzy.com/filter/clear_filter/all">Clear All</a>
+                </div>
+                <div class="listing-top-controls d-block d-md-none">
+                    <div class="dropdown show float-left">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMobileListing" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-angle-down"></i>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMobileListing" rel="dropdownMobileListing">
+                            <li><a class="dropdown-item" href="#">Accent</a></li>
+                            <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Living<i class="fas fa-angle-right float-right"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">All</a></li>
+                                    <li><a class="dropdown-item" href="#">Sofa</a></li>
+                                    <li><a class="dropdown-item" href="#">Seating</a></li>
+                                    <li><a class="dropdown-item" href="#">Table</a></li>
+                                    <li><a class="dropdown-item" href="#">Remaining</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="dropdown-item" href="#">Bed</a></li>
+                            <li><a class="dropdown-item" href="#">Kitchen</a></li>
+                            <li><a class="dropdown-item" href="#">Remaining</a></li>
+                        </ul>
+                    </div>
+                    {{ Breadcrumbs::render('products') }}
+                    <span class="filter-toggle float-right" id="filterToggleBtn">
+                        <i class="fas fa-filter"></i>
+                    </span>
+                    <span class="view-items-toggle float-right" id="viewItemsBtn">
+                        <i class="fab fa-buromobelexperte"></i>
+                    </span>
+                </div>
+                <div class="products-container col-md-10">
+                    <div class="float-right d-none d-md-block">
+                        <div class="sortby">
+                            <label for="sort">Sort By</label>
+                            <select class="form-control" id="sort">
+                                <option>Price : Low to High</option>
+                                <option>Price : High to Low</option>
+                                <option>Popularity</option>
+                                <option selected>Recommended</option>
+                            </select>
+                        </div>
+                        <div class="total-items"><span id="totalResults">0</span> Results</div>
+                    </div>
+                    <div class="ls-prod-container container-fluid text-center">
+                        <div class="row" id="productsContainerDiv">
+                        </div>
+                        <div class="text-center" id="noProductsText">Sorry, no more products to show.</div>
+                        <div class="mx-auto" id="loaderImg">
+                            <img src="{{ asset('/images/Spinner-1s-100px.gif') }}" alt="Spinner">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
+@push('pageSpecificScripts')
+    <script src="{{ mix('js/search.js')}}"></script>
+@endpush
