2f870e5ca4b15ee380e29e48ab6d7d48bc528ce3
Aalind Sharma <aalind.01@gmail.com>
2019-09-13 01:49:00 +0530

    Fix duplicate swatches issue

--- resources/js/pages/detailspage.js
+++ resources/js/pages/detailspage.js
@@ -220,6 +220,20 @@ $(document).ready(function () {
         var variationSwatchImages = variationData.map(( variation, idx) => { 
             return variation.swatch_image || variationImages[idx];
         });
+        var arrDupes = [];
+        var variationSwatchImagesNew = variationSwatchImages.filter(function(item, index,self){
+            if( self.indexOf(item) === index ){
+                arrDupes.push(index);
+            }
+            return self.indexOf(item) === index;
+        });
+        console.log(arrDupes);
+        var variationImagesNew = variationImages.filter(function(item, index){
+            return arrDupes.indexOf(index) >= 0
+        });
+        // for( var i=0; i< arrDupes.length ; i++){
+        //     variationImages.splice(i,1);
+        // }
         var variationLinks = variationData.map(variation => variation.link);
 
         var $variationsCarousel = $product.find('.-variations-carousel');
@@ -228,13 +242,13 @@ $(document).ready(function () {
             class: 'responsive',
         }).appendTo($variationsCarousel);
 
-        variationSwatchImages.forEach((img, idx) => {
+        variationSwatchImagesNew.forEach((img, idx) => {
             var responsiveImgDiv = jQuery('<div/>', {
                 class: 'mini-carousel-item',
             }).appendTo(carouselMainDiv);
             var anchor = jQuery('<a/>', {
                 class: 'responsive-img-a',
-                "data-image": variationImages[idx] ? variationImages[idx] : '',
+                "data-image": variationImagesNew[idx] ? variationImagesNew[idx] : '',
             }).appendTo(responsiveImgDiv);
             var responsiveImg = jQuery('<img/>', {
                 class: 'zoom-img carousel-img img-fluid',
