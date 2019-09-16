05ecd37e8b324cc0aa1efd1969afcad0f81a5043
Aalind Sharma <aalind.01@gmail.com>
2019-09-16 10:26:17 +0530

    Update privacy policy page.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <span class="copyright float-left">&copy; 2019 LazySuzy</span>
     <span class="policy-terms float-right">
-        <a href="//getterms.io/g/sdPaTTm">Privacy Policy</a>
+        <a href="/privacypolicy">Privacy Policy</a>
         <span>|</span>
         <a href="#">Terms of service</a>
     </span>
