0a5e986e017ae2703bc7ad76103c05b81a0a8557
Aalind Sharma <aalind.01@gmail.com>
2019-09-17 10:42:10 +0530

    Add aboutus page.

---
+++ resources/views/pages/aboutus.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout', ['body_class' => 'aboutus-main-div'])
+
+@section('middle_content')
+<div class="aboutus container">
+    <h2 class="main-heading">Furnish your home. From your home.</h2>
+    <h3 class="secondary-heading">We are a search engine for your Home.</h3>
+    <p>We don't sell furniture ourselves. We just bring you the Highest Quality Brands all in one Place.</p>
+    <p>You need a Couch. Maybe you know exactly what you want it to look like. Or you just want some inspiration.</p>
+    <p>Either way, you don't need to spend days & days combing the web for the perfect design or the best deal.<p>
+    <p>LazySuzy was born for people like you and me who could use a better way to furnish our home online.<p>
+    <p>We're just a team of regular guys & gals that want to help you search for & discover your dream furnishings without having to pay for it.<p>
+    <p>We're here to make your life easier. Reach out and let us know how we can do more to help.<p>
+</div>
+@endsection
