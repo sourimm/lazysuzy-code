3220189903e1877838b25ae3b6402e7099e769aa
Himanshu <emailfromhimanshu@gmail.com>
2019-10-03 23:50:23 +0530

    ui and ordering

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -4,7 +4,7 @@
         @include('./partials/subnav')
 
         <div class="d-block d-md-none controls-div">
-            
+
             <div class="wishlist-icon float-right m-10" id="wishlistBtn">
                 <i class="far fa-heart -icon"></i>
             </div>
@@ -15,43 +15,55 @@
          <div class="category-img-container">
              <div class="row">
              <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Benches.jpg" alt="Benches" >  
-                <span> Benches</span>  
-             </div>
-             <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Bookshelf.jpg" alt="BookSelf" > 
-                <span> BookShelves & Cabinets</span>   
-             </div>
-             <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Chairs.jpg" alt="Charis" >
-                <span> Chairs</span>     
-             </div>
-             <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Sofa.jpg" alt="Sofa" >  
-                <span> Sofas</span>   
-             </div>
-         </div>
-         <div class="row">
-             <div class="col-3 category-text ">
-                <img class="category-img" src="/images/product-category/CoffeeTable.jpg" alt="CoffeeTable" > 
-                <span> Coffee & Side Table</span>    
-             </div>
-              <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Loveseat.jpg" alt="BookSelf" > 
-                <span>Loveseats & Daybeds</span>    
-             </div>
+             <a href="/products/living/sofa">
+                <img class="category-img" src="/images/product-category/Sofa.jpg" alt="Sofa" >
+                <span> Sofas</span>
+                </a>
+            </div>
+            <div class="col-3 category-text">
+                <a href="/products/living/sectionals">
+                    <img class="category-img" src="/images/product-category/Sectionals.jpg" alt="Sectionals" >
+                    <span> Sectionals</span>
+                </a>
+            </div>
+            <div class="col-3 category-text">
+                <img class="category-img" src="/images/product-category/Loveseat.jpg" alt="BookSelf" >
+                <span>Loveseats & Daybeds</span>
+            </div>
+            <div class="col-3 category-text">
+                <a href="/products/living/chairs">
+                    <img class="category-img" src="/images/product-category/Chairs.jpg" alt="Charis" >
+                    <span> Chairs</span>
+                </a>
+            </div>
+
              <div class="col-3 category-text">
                 <img class="category-img" src="/images/product-category/Ottoman.jpg" alt="Ottoman" >
-                <span> Ottoman, Poufs & Stools</span>     
+                <span> Ottoman, Poufs & Stools</span>
              </div>
-            <div class="col-3 category-text">
-                <img class="category-img" src="/images/product-category/Sectionals.jpg" alt="Sectionals" >   
-                <span> Sectionals</span>  
+             <div class="col-3 category-text">
+                <a href="/products/living/benches">
+                    <img class="category-img" src="/images/product-category/Benches.jpg" alt="Benches" >
+                    <span> Benches</span>
+                </a>
              </div>
+             <div class="col-3 category-text ">
+                <a href="/products/living/tables">
+                    <img class="category-img" src="/images/product-category/CoffeeTable.jpg" alt="CoffeeTable" >
+                    <span> Coffee & Side Table</span>
+                </a>
+             </div>
+             <div class="col-3 category-text">
+                <a href="/products/living/storage">
+                    <img class="category-img" src="/images/product-category/Bookshelf.jpg" alt="BookSelf" >
+                    <span> BookShelves & Cabinets</span>
+                </a>
+             </div>
+
          </div>
          </div>
 
-        
+
 
 
 
