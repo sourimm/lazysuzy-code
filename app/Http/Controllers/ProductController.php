18b6daa16f9ed398ea657ee0b899f50c3fbf4554
Himanshu <emailfromhimanshu@gmail.com>
2019-10-07 18:39:26 +0530

    api integrated on department page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,8 +15,12 @@ class ProductController extends Controller
         $departments = Department::all();
         $products = Product::paginate(20);
         $categories = DepartmentMapping::get();
-        if ($request->dept=='living' &&$request->cat== '') {
-            return view('pages.category');
+        $singleDepartment = Department::get_single_department($request->dept);
+        $departmentName = $singleDepartment['department'];
+        $listedCategories = $singleDepartment['categories'];
+
+        if (($request->dept=='living'|| $request->dept=='bedroom') &&$request->cat== '') {
+            return view('pages.category', compact('departmentName', 'listedCategories'));
         }
         if ($request->ajax()) {
             return $products;
--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -14,55 +14,15 @@
         </div>
          <div class="category-img-container">
              <div class="row">
-             <div class="col-3 category-text">
-             <a class="category-text" href="/products/living/sofa">
-                <img class="category-img" src="/images/product-category/Sofa.jpg" alt="Sofa" >
-               <div><span>Sofas</span></div>
-                </a>
-            </div>
-            <div class="col-3 category-text">
-                <a class="category-text" href="/products/living/sectionals">
-                    <img class="category-img" src="/images/product-category/Sectionals.jpg" alt="Sectionals" >
-                    <div><span>Sectionals</span></div>
-                </a>
-            </div>
-            <div class="col-3 category-text">
-            <a class="category-text" href="/products/living/loveseat">
-                <img class="category-img" src="/images/product-category/Loveseat.jpg" alt="BookSelf" >
-                <div><span>Loveseats & Daybeds</span></div>
-</a>
-            </div>
-            <div class="col-3 category-text">
-                <a class="category-text" href="/products/living/chairs">
-                    <img class="category-img" src="/images/product-category/Chairs.jpg" alt="Charis" >
-                   <div> <span> Chairs</span></div>
-                </a>
-            </div>
+             @foreach ($listedCategories as $category)
+                <div class="col-3 category-text">
+                    <a class="category-text" href="{{$category['link']}}">
+                        <img class="category-img" src="{{$category['image']}}" alt="{{$category['category']}}" >
+                        <div><span>{{$category['category']}}</span></div>
+                    </a>
+                </div>
+            @endforeach
 
-             <div class="col-3 category-text">
-             <a class="category-text" href="/products/living/ottomans">
-                <img class="category-img" src="/images/product-category/Ottoman.jpg" alt="Ottoman" >
-              <div><span> Ottoman, Poufs & Stools</span></div>
-</a>
-             </div>
-             <div class="col-3 category-text">
-                <a class="category-text" href="/products/living/benches">
-                    <img class="category-img" src="/images/product-category/Benches.jpg" alt="Benches" >
-                  <div><span> Benches</span></div>
-                </a>
-             </div>
-             <div class="col-3 category-text ">
-                <a class="category-text" href="/products/living/tables">
-                    <img class="category-img" src="/images/product-category/CoffeeTable.jpg" alt="CoffeeTable" >
-                   <div><span> Coffee & Side Table</span></div>
-                </a>
-             </div>
-             <div class="col-3 category-text">
-                <a class="category-text" href="/products/living/storage">
-                    <img class="category-img" src="/images/product-category/Bookshelf.jpg" alt="BookSelf" >
-                  <div><span> BookShelves & Cabinets</span></div>
-                </a>
-             </div>
 
          </div>
          </div>
