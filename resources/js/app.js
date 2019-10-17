ded51c962d70294c6c4fa25bb0a7719a7e9f903a
Himanshu <emailfromhimanshu@gmail.com>
2019-10-17 17:57:40 +0530

    bug fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,112 +1,156 @@
-require('bootstrap');
-require('slick-carousel');
-require('./components/multi-carousel');
-require('./components/custom-selectbox');
+require('bootstrap')
+require('slick-carousel')
+require('./components/multi-carousel')
+require('./components/custom-selectbox')
 
-$(document).ready(function () {
-  $('#searchbarHeader').submit(function(e){
-    callSearch(e,this);
-  });
+$(document).ready(function() {
+    $('#departmentsNav').on('click', '.dropdown', function(e) {
+        console.log('test')
+        // e.preventDefault()
+        $(this)
+            .siblings()
+            .removeClass('active')
+        $(this).addClass('active')
+    })
+    $('#searchbarHeader').submit(function(e) {
+        callSearch(e, this)
+    })
 
-  $('.sb-body').submit(function(e){
-      callSearch(e,this);
-  });
-
-  function callSearch(e, elm){
-      e.preventDefault();
-      window.location.href = '/search?query='+$(elm).find('input').val(); //relative to domain
-  }
-
-  var $searchIcon = $('#searchIconMobile');
-
-  const DEPT_API = '/api/all-departments'
-
-  $searchIcon.on('click', function (e) {
-    if ($(this).attr('id') == 'searchIconMobile') {
-      if ($('#searchbarHeader').hasClass('open')) {
-        $('#searchbarHeader').removeClass('open');
-      } else {
-        $('#searchbarHeader').addClass('open');
-      }
-    }  
-  });
-
-  $('.user-login-modal').click(function() {
-      $('#modalLoginForm').modal();
-  });
-
-  $('.wishlist-login-modal').click(function() {
-    $('#modalLoginForm').modal();
-  });
-
-  $('body').on("mouseover", '.dropdown-submenu', function (e) {
-    var self = this;
-    $('.dropdown-submenu').each(function () {
-      if ($(this).find('.dropdown-menu')[0] != $(self).next('ul')[0]) {
-        $(this).find('.dropdown-menu').hide();
-      }
-    });
-    $(this).find('ul').toggle();
-    if( !isMobile() ){
-      $(this).find('.dropdown-menu').css('top', $(this).position().top);
+    $('.sb-body').submit(function(e) {
+        callSearch(e, this)
+    })
+    function callSearch(e, elm) {
+        e.preventDefault()
+        window.location.href =
+            '/search?query=' +
+            $(elm)
+                .find('input')
+                .val() //relative to domain
     }
-  });
 
-  $.ajax({
-    type: "GET",
-    url: DEPT_API,
-    dataType: "json",
-    success: function (departments) {
-      var deptToAppend = '';
-      if (isMobile()) {
-        var singleDeptMobile = '';
-        for (var i = 0; i < departments.length; i++) {
-          if (departments.length != 0) {
-            singleDeptMobile = '<div class="col-4 col-sm-auto -dept "><a href="' + departments[i].link + '">' + departments[i].department + '</a></div>';
-          }
-          $('#mobileDepartments').append(singleDeptMobile);
-        }
-      }
-      for (var i = 0; i < departments.length; i++) {
-        if (departments[i].categories.length == 0) {
-          deptToAppend += '<li><a href="' + departments[i].link + '">' + departments[i].department + '</a></li>';
-        }
-        else {
-          deptToAppend += '<li class="dropdown"><a  href="'+departments[i].link+'" id="navbarDropdown'+i+'" role="button"  aria-haspopup="true" aria-expanded="false">' + departments[i].department + '</a>';
-          var catgToAppend = '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
-          for (var j = 0; j < departments[i].categories.length; j++) {
-            // if (departments[i].categories[j].sub_categories.length == 0) {
-              catgToAppend += '<li><a href="' + departments[i].categories[j].link + '">' + departments[i].categories[j].category + '</a></li>'
-            // }
-            // else {
-            //   catgToAppend += '<li class="dropdown-submenu">';
-            //   catgToAppend += '<a href="'+departments[i].categories[j].link+'">' + departments[i].categories[j].category + '<span class="mx-2"><i class="fas fa-angle-right"></i></span>';
-            //   var subcatToAppend = '<ul class="dropdown-menu">';
-            //   for (k = 0; k < departments[i].categories[j].sub_categories.length; k++) {
-            //     subcatToAppend += '<li><a href="' + departments[i].categories[j].sub_categories[k].link + '">' + departments[i].categories[j].sub_categories[k].sub_category + '</a></li>'
-            //   }
+    var $searchIcon = $('#searchIconMobile')
 
-            //   subcatToAppend += '</ul>';
-            //   catgToAppend += subcatToAppend;
-            //   catgToAppend += '</li>';
-            // }
-          }
-          catgToAppend += '</ul>';
-          deptToAppend += catgToAppend;
-          deptToAppend += '</li>';
-        }
-      }
-      $('#departmentsNav').append(deptToAppend);
+    const DEPT_API = '/api/all-departments'
 
-    },
-    error: function (jqXHR, exception) {
-      console.log(jqXHR);
-      console.log(exception);
-    }
-  });
+    $searchIcon.on('click', function(e) {
+        if ($(this).attr('id') == 'searchIconMobile') {
+            if ($('#searchbarHeader').hasClass('open')) {
+                $('#searchbarHeader').removeClass('open')
+            } else {
+                $('#searchbarHeader').addClass('open')
+            }
+        }
+    })
+
+    $('.user-login-modal').click(function() {
+        $('#modalLoginForm').modal()
+    })
+
+    $('.wishlist-login-modal').click(function() {
+        $('#modalLoginForm').modal()
+    })
+
+    $('body').on('mouseover', '.dropdown-submenu', function(e) {
+        var self = this
+        $('.dropdown-submenu').each(function() {
+            if ($(this).find('.dropdown-menu')[0] != $(self).next('ul')[0]) {
+                $(this)
+                    .find('.dropdown-menu')
+                    .hide()
+            }
+        })
+        $(this)
+            .find('ul')
+            .toggle()
+        if (!isMobile()) {
+            $(this)
+                .find('.dropdown-menu')
+                .css('top', $(this).position().top)
+        }
+    })
+
+    $.ajax({
+        type: 'GET',
+        url: DEPT_API,
+        dataType: 'json',
+        success: function(departments) {
+            var deptToAppend = ''
+            if (isMobile()) {
+                var singleDeptMobile = ''
+                for (var i = 0; i < departments.length; i++) {
+                    if (departments.length != 0) {
+                        singleDeptMobile =
+                            '<div class="col-4 col-sm-auto -dept "><a href="' +
+                            departments[i].link +
+                            '">' +
+                            departments[i].department +
+                            '</a></div>'
+                    }
+                    $('#mobileDepartments').append(singleDeptMobile)
+                }
+            }
+            for (var i = 0; i < departments.length; i++) {
+                if (departments[i].categories.length == 0) {
+                    deptToAppend +=
+                        '<li><a href="' +
+                        departments[i].link +
+                        '">' +
+                        departments[i].department +
+                        '</a></li>'
+                } else {
+                    let classActive =
+                        departments[i].link === location.pathname
+                            ? 'active'
+                            : ''
+                    deptToAppend +=
+                        '<li class="dropdown ' +
+                        classActive +
+                        '"><a  href="' +
+                        departments[i].link +
+                        '" id="navbarDropdown' +
+                        i +
+                        '" role="button"  aria-haspopup="true" aria-expanded="false">' +
+                        departments[i].department +
+                        '</a>'
+                    var catgToAppend =
+                        '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">'
+                    for (var j = 0; j < departments[i].categories.length; j++) {
+                        // if (departments[i].categories[j].sub_categories.length == 0) {
+                        catgToAppend +=
+                            '<li><a href="' +
+                            departments[i].categories[j].link +
+                            '">' +
+                            departments[i].categories[j].category +
+                            '</a></li>'
+                        // }
+                        // else {
+                        //   catgToAppend += '<li class="dropdown-submenu">';
+                        //   catgToAppend += '<a href="'+departments[i].categories[j].link+'">' + departments[i].categories[j].category + '<span class="mx-2"><i class="fas fa-angle-right"></i></span>';
+                        //   var subcatToAppend = '<ul class="dropdown-menu">';
+                        //   for (k = 0; k < departments[i].categories[j].sub_categories.length; k++) {
+                        //     subcatToAppend += '<li><a href="' + departments[i].categories[j].sub_categories[k].link + '">' + departments[i].categories[j].sub_categories[k].sub_category + '</a></li>'
+                        //   }
+
+                        //   subcatToAppend += '</ul>';
+                        //   catgToAppend += subcatToAppend;
+                        //   catgToAppend += '</li>';
+                        // }
+                    }
+                    catgToAppend += '</ul>'
+                    deptToAppend += catgToAppend
+                    deptToAppend += '</li>'
+                }
+            }
+            $('#departmentsNav').append(deptToAppend)
+        },
+        error: function(jqXHR, exception) {
+            console.log(jqXHR)
+            console.log(exception)
+        }
+    })
 })
 
-export default function isMobile(){
-  var isMobile = window.matchMedia("only screen and (max-width: 768px)");
-  return isMobile.matches ? true : false
+export default function isMobile() {
+    var isMobile = window.matchMedia('only screen and (max-width: 768px)')
+    return isMobile.matches ? true : false
 }
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,6 +9,18 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126036769-1"></script>
     <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-126036769-1');</script>
+     <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PD8TXBM');</script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PD8TXBM"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script type="text/javascript">
         var vglnk = {key: '7c7cd49fe471830c75c9967f05d5f292'};
         (function(d, t) {
@@ -64,6 +76,8 @@
         g.clearUserCookie=function(){};
         })(window,document,window['_fs_namespace'],'script','user');
     </script>
+
+
 
     @endif
 
