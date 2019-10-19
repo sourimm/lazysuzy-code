eb693864c1286f2c835d77182975e2b8ae2b7ddf
Tarun <emailfromhimanshu@gmail.com>
2019-10-19 23:38:18 +0530

    minnor fixes

--- resources/views/components/signmodal.blade.php
+++ resources/views/components/signmodal.blade.php
@@ -74,7 +74,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn -btn">
                                     {{ __('Register') }}
                                 </button>
