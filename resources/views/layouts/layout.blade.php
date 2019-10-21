68b5ab2d4db42c5ad64c5dd5055b61c9344a1a06
Himanshu <emailfromhimanshu@gmail.com>
2019-10-21 23:48:04 +0530

    full story code fix

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -55,6 +55,7 @@
     </script>
 
     <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/es5imqpc';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+    @endif
 
     <script>
         window['_fs_debug'] = false;
@@ -78,7 +79,7 @@
 
 
 
-    @endif
+
 
 
     @stack('pageSpecificStyles')
@@ -115,8 +116,8 @@
         @component('components.authmodal')
         @endcomponent
     </div>
-        
-        
+
+
     </div>
 
 
