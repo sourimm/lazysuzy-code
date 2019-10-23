8142381cdd92d86e76ed510af6c2fe83553ab368
Tarun <emailfromhimanshu@gmail.com>
2019-10-23 13:51:22 +0530

    modal ui issues fixed

--- resources/views/components/detailOverview.blade.php
+++ resources/views/components/detailOverview.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade product-modal" id="modalProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header border-bottom-0">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -41,6 +41,7 @@
                     </div>
                 </div>
             </div>
+            <hr/>
 
                 <h4 class="-name">product name</h4>
 
@@ -49,7 +50,7 @@
                 <div class="rating-container"><div class="rating"></div><span class="total-ratings"></span></div>
     </div>
     <div class="all-desc">
-
+<hr/>
     <nav class="navbar navbar-light bg-light">
         <ul class="nav nav-pills">
             <li class="nav-item">
@@ -64,17 +65,23 @@
 
         </ul>
         </nav>
+        <hr/>
         <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
         <h4 id="fat">@fat</h4>
+        <hr/>
         <p class="-desc d-none d-md-block"></p>
         <h4 id="mdo">@mdo</h4>
         <ul class="-features"></ul>
+        <hr/>
         <h4 id="one">one</h4>
         <p>...</p>
+        <hr/>
         <h4 id="two">two</h4>
         <p>...</p>
+        <hr/>
         <h4 id="three">three</h4>
         <p>...</p>
+        
         </div>
 </div>
 
