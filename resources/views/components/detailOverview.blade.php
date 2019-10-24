e0181f1497555962d01712759fcdd1ee2e0072ff
Himanshu <emailfromhimanshu@gmail.com>
2019-10-24 19:12:13 +0530

    product modal updated

--- resources/views/components/detailOverview.blade.php
+++ resources/views/components/detailOverview.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade product-modal" id="modalProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header border-bottom-0">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -20,23 +20,19 @@
         <i class="fas fa-filter -icon"></i>
     </div>
 </div>
-
-
 <div class="row container box">
     <div class="col-12 col-md-6"><div class="-images-container" id="scroll-bar-img"><div class="-images" ></div></div></div>
-
-
     <div class="prod-desc col-12 col-md-6">
         <div>
             <div class="prod-main-img  order-md-last">
             </div>
             <div class="brand-header">
                 <div class="row">
-                    <h2 class="col-8 -site">Brand name</h2>
-                    <div class="col-2 m-10" id="wishlistBtnDesktop">
+                    <h2 class="col-8  -site">Brand name</h2>
+                    <div class="col-2 mt-2" id="wishlistBtnDesktop">
                         <i class="far fa-heart -icon"></i> Save
                     </div>
-                    <div class="col-2">
+                    <div class="col-2 mt-2">
                     <i class="fa fa-share -icon"></i> Share
                     </div>
                 </div>
@@ -53,13 +49,13 @@
 <hr class="modal-hr"/>
     <nav class="navbar-modal navbar-light bg-light">
         <ul class="nav nav-pills">
-            <li class="modal-nav-item">
+            <li class="modal-nav-item pl-4 pr-4">
             <a class="modal-nav-link" href="#fat">Overview</a>
             </li>
-            <li class="modal-nav-item">
+            <li class="modal-nav-item pl-4 pr-4">
             <a class="modal-nav-link" href="#mdo">Features</a>
             </li>
-            <li class="modal-nav-item">
+            <li class="modal-nav-item pl-4 pr-4">
             <a class="modal-nav-link" href="#mdo">Dimensions</a>
             </li>
 
@@ -74,22 +70,15 @@
         <h4>Features</h4>
         <hr class="modal-hr"/>
         <ul class="-features"></ul>
+        <hr class="modal-hr"/>
+        <h4>Dimension</h4>
+        <hr class="modal-hr"/>
+        <ul class="-dimension"></ul>
         </div>
 </div>
-
-
-
 </div>
-
-
 </div>
-
-
-
       </div>
-
-
-
     </div>
   </div>
 </div>
