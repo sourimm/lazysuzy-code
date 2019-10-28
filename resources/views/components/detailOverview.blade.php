27b551f92a13e411b34589f21819e608045d81ec
Tarun <emailfromhimanshu@gmail.com>
2019-10-28 17:52:40 +0530

    modal issues fixed on desktop view

--- resources/views/components/detailOverview.blade.php
+++ resources/views/components/detailOverview.blade.php
@@ -1,10 +1,10 @@
 <div class="modal fade product-modal" id="modalProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <button type="button" class="product-close close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
-    <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
+    <div class="product-modal-dialog modal-dialog modal-xl" role="document">
+        <div class="product-modal-content modal-content">
+            <div class="product-moadal-body modal-body">
                 <div class="detailspage" id="detailPage">
                     <div class="d-block d-md-none controls-div">
                         <div class="wishlist-icon float-right m-10" id="wishlistBtn">
@@ -14,8 +14,8 @@
                             <i class="fas fa-filter -icon"></i>
                         </div>
 </div>
-<div class="row container box">
-    <div class="col-12 col-md-6"><div class="-images-container" id="scroll-bar-img"><div class="-images" ></div></div></div>
+<div class="row">
+    <div class="col-12 col-md-6"><div class="-images-container" id="scroll-bar-img"><div class="-images" id="scroll-bar-img"></div></div></div>
     <div class="prod-desc col-12 col-md-6">
         <div>
             <div class="prod-main-img  order-md-last">
@@ -24,49 +24,44 @@
                 <div class="row">
                
                     <h2 class="col-8  -site">Brand name</h2>
-                    <div class="col-2 mt-2" id="wishlistBtnDesktop">
+                    <!-- <div class="col-2 mt-2" id="wishlistBtnDesktop">
                         <i class="far fa-heart -icon"></i> Save
-                    </div>
-                    <div class="col-2 mt-2 btn-copy">
+                    </div> -->
+                    <!-- <div class="col-2 mt-2 btn-copy">
                     <i class="fa fa-share -icon"></i> Share
-                    </div>
+                    </div> -->
                 </div>
             </div>
-            <hr class="modal-hr" />
-
-                <h4 class="-name">product name</h4>
-
-
-                <div class="rating-container"><div class="rating"></div><span class="total-ratings"></span></div>
-                <div class="prod-price-card d-none d-md-block">
-    </div>
-    <div class="all-desc">
-<hr class="modal-hr"/>
-    <nav class="navbar-modal navbar-light bg-light">
-        <ul class="nav nav-pills">
-            <li class="modal-nav-item pl-4 pr-4">
-            <a class="modal-nav-link" href="#Overview">Overview</a>
-            </li>
-            <li class="modal-nav-item pl-4 pr-4">
-            <a class="modal-nav-link" href="#Features">Features</a>
-            </li>
-            <li class="modal-nav-item pl-4 pr-4">
-            <a class="modal-nav-link" href="#Dimensions">Dimensions</a>
-            </li>
-
-        </ul>
+                    <hr class="modal-hr" />
+                    <h4 class="-name">product name</h4>
+                    <div class="rating-container"><div class="rating"></div><span class="total-ratings"></span></div>
+                    <div class="prod-price-card d-none d-md-block"></div>
+                    <div class="all-desc">
+                        <hr class="modal-hr"/>
+                        <nav class="navbar-modal navbar-light bg-light">
+                        <ul class="nav nav-pills">
+                            <li class="modal-nav-item pl-4 pr-4">
+                                <a class="modal-nav-link" href="#Overview">Overview</a>
+                            </li>
+                            <li class="modal-nav-item pl-4 pr-4">
+                                <a class="modal-nav-link" href="#Features">Features</a>
+                            </li>
+                            <li class="modal-nav-item pl-4 pr-4">
+                                <a class="modal-nav-link" href="#Dimensions">Dimensions</a>
+                            </li>
+                        </ul>
         </nav>
         <hr class="modal-hr"/>
         <div class="products-modal-details" data-spy="scroll" data-target="#navbar-example2" data-offset="0">
-        <h4 id="#Overview">Overview</h4>
+        <h4 id="Overview">Overview</h4>
         <hr class="modal-hr"/>
         <p class="-desc d-none d-md-block"></p>
         <hr class="modal-hr"/>
-        <h4 id="#Features">Features</h4>
+        <h4 id="Features">Features</h4>
         <hr class="modal-hr"/>
         <ul class="-features"></ul>
         <hr class="modal-hr"/>
-        <h4 id="#Dimension">Dimension</h4>
+        <h4 id="Dimensions">Dimension</h4>
         <hr class="modal-hr"/>
         <ul class="-dimension"></ul>
         </div>
