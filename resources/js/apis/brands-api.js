a20fc950cb354da339e908904326d0f8e0c3b81a
Tarun <emailfromhimanshu@gmail.com>
2019-11-22 01:58:40 +0530

    dynamic brands page

---
+++ resources/js/apis/brands-api.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/products/living/sofa?filters=&sort_type=&pageno=0',
+        dataType: 'json',
+        success: function(data) {
+            productDiv(data)
+        },
+        error: function(jqXHR, exception) {
+            console.log(jqXHR)
+            console.log(exception)
+        }
+    })
+    debugger
+    function productDiv(data) {
+        data.products.slice(0, 7).map(product => {
+            // data.products.map(product => {
+            let brands_prod_div = jQuery('<div/>', {
+                class: 'brands-prod-div col-3 '
+            }).appendTo('#product-div-main')
+            let brands_prod = jQuery('<div/>', {
+                class: 'brands-product'
+            }).appendTo(brands_prod_div)
+            let brands_img = jQuery('<img/>', {
+                class: 'brands-product-img',
+                src: product.main_image
+            }).appendTo(brands_prod)
+            let prod_info = jQuery('<div/>', {
+                class: 'prod-info'
+            }).appendTo(brands_prod_div)
+            let prod_name = jQuery('<span/>', {
+                class: 'float-right',
+                html: `${product.name}`
+            }).appendTo(prod_info)
+            let prod_price = jQuery('<span/>', {
+                class: 'float-left',
+                html: `$${product.is_price}`
+            }).appendTo(prod_info)
+        })
+    }
+})
--- resources/views/pages/brands.blade.php
+++ resources/views/pages/brands.blade.php
@@ -3,24 +3,106 @@
 @section('middle_content')
 @include('./partials/subnav')
     <div class="brands">
-    <div class="d-none d-md-block">
+        <div class="main-img">
+            <img class="img-fluid" src="{{ asset('/images/Floyd.jpg') }}" alt="">
+        </div>   
+        <div class="brands-main-heading">
+            <div class="container">
+            <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>Floyd</h1>
+                    </div>
+                    <div class="row">
+                    <div class=" col-8">
+                    <div class="brands-details">
+                    <h3>Floyd began because we were tried of disposal furniture. 
+                        So we set out to design products of lasting quality fo how people live today. 
+                        Furniture should be made for the home, not the landfill.
+                        Made with material that last. It’s different way of making furniture.
+                        we cal it furniture for keeping. </h3>
+                    </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-12 ">  
+                               <div class="row">
+                                   <div class="col-3 ">
+                                   <img class="brands-icon" src="{{ asset('/images/icon/heart-flag-of-united-states-of-america.png') }}" alt="">
+                                   </div>
+                                   <div class="col-9 ">
+                                       <h3>Manufactured in USA</h3>
+                                   </div>
+                               </div>
+                            </div>
+                            <div class="col-12 ">  
+                               <div class="row">
+                                   <div class="col-3 ">
+                                   <img class="brands-icon" src="{{ asset('/images/icon/shipping.png') }}" alt="">
+                                   </div>
+                                   <div class="col-9 ">
+                                       <h3>Free Shipping</h3>
+                                   </div>
+                               </div>
+                            </div>
+                          
+                            <div class="col-12 ">  
+                               <div class="row">
+                                   <div class="col-3 ">
+                                   <img class="brands-icon" src="{{ asset('/images/icon/exchange.png') }}" alt="">
+                                   </div>
+                                   <div class="col-9 ">
+                                       <h3>Free 30 Days Returns</h3>
+                                   </div>
+                               </div>
+                            </div>
+                          
+                            <div class="col-12 ">  
+                               <div class="row">
+                                   <div class="col-3 ">
+                                   <img class="brands-icon" src="{{ asset('/images/icon/guarantee.png') }}" alt="">
+                                   </div>
+                                   <div class="col-9 ">
+                                       <h3>10 years Warranty</h3>
+                                   </div>
+                               </div>
+                            </div>                       
+                        </div>
+                    </div>
+                </div>            
             </div>
-         
-     
-    </div>
-    <!-- <div class="listing">
-        <div class="listing-container main-container container ">
-            <div class="d-none d-md-block">
-            </div>
-         
-
-
-                </div>
-            
-
-
-               
         </div>
-     
-    </div>
- -->
+    </div>  
+    <hr class="brands-hr">
+    <div class="brands-prod-container">
+        <div class="container">
+            <div id="product-div-main" class="row"></div>
+        <!-- <div class="row product-div-main">
+           <div class="brands-prod-div col-3 ">
+                <div class="brands-product">
+                    <img class="brands-product-img" src="{{ asset('/images/Floyd.jpg') }}" alt="">
+                    <div class="prod-info">
+                        <span class="float-left">$ 1000</span>
+                        <span class="float-right">Westelm</span>
+                    </div>
+                </div>
+                <div class="product-detail">
+                    <p>Mirrored Silver Chest & Dresser Bedroom Set</p>
+                </div>
+            </div>
+            
+           </div>
+        </div> -->
+    </div>          
+    @include('./partials/brandassociation')
+            <!-- </div>
+        </div>    
+        
+    </div> -->
+    <!-- @include('./partials/brandassociation')  -->
+@endsection
+    <!-- <script src="{{ ('./js/apis/brands-api.js')}}"></script> -->
+    <!-- @push('pageSpecificScripts')
+    <script src="{{ mix('js/brands.js')}}"></script>
+ <script src="{{ mix('js/detailOverview.js')}}"></script> -->
+@endpush -->
+
