dfca27f6276f5705a3eaa4d22433726b7c416a38
Himanshu <emailfromhimanshu@gmail.com>
2019-11-22 23:09:32 +0530

    brand feedback changes

--- resources/views/pages/brands.blade.php
+++ resources/views/pages/brands.blade.php
@@ -5,7 +5,7 @@
     <div class="brands">
         <div class="main-img">
             <img class="img-fluid" src="{{ asset('/images/Floyd.jpg') }}" alt="">
-        </div>   
+        </div>
         <div class="brands-main-heading">
             <div class="container">
             <div class="row">
@@ -15,64 +15,84 @@
                     <div class="row">
                     <div class="col-8 col-sm-12">
                     <div class="brands-details">
-                    <h3>Floyd began because we were tried of disposal furniture. 
-                        So we set out to design products of lasting quality fo how people live today. 
+                    <h3>Floyd began because we were tired of disposable furniture.
+                        So we set out to design products of lasting quality fo how people live today.
                         Furniture should be made for the home, not the landfill.
-                        Made with material that last. It’s different way of making furniture.
-                        we cal it furniture for keeping. </h3>
+                        Made with material that last. It’s a different way of making furniture.
+                        we call it furniture for keeping. </h3>
                     </div>
                     </div>
                     <div class="col-4 col-sm-12">
                         <div class="row">
-                            <div class="col-12 col-sm-6">  
+                            <div class="col-12 col-sm-6">
                                <div class="row">
                                    <div class="col-3 col-sm-12">
                                    <img class="brands-icon" src="{{ asset('/images/icon/heart-flag-of-united-states-of-america.png') }}" alt="">
                                    </div>
+<<<<<<< HEAD
                                    <div class="col-9 col-sm-12">
+=======
+                                   <div class="col-9 col-sm-12 brand-icon-text">
+>>>>>>> shadow
                                        <h3>Manufactured in USA</h3>
                                    </div>
                                </div>
                             </div>
-                            <div class="col-12 col-sm-6 ">  
+                            <div class="col-12 col-sm-6 ">
                                <div class="row">
                                    <div class="col-3 col-sm-12">
                                    <img class="brands-icon" src="{{ asset('/images/icon/shipping.png') }}" alt="">
                                    </div>
+<<<<<<< HEAD
                                    <div class="col-9 col-sm-12">
+=======
+                                   <div class="col-9 col-sm-12 brand-icon-text">
+>>>>>>> shadow
                                        <h3>Free Shipping</h3>
                                    </div>
                                </div>
                             </div>
-                          
-                            <div class="col-12 col-sm-6">  
+
+                            <div class="col-12 col-sm-6">
                                <div class="row">
                                    <div class="col-3 col-sm-12">
                                    <img class="brands-icon" src="{{ asset('/images/icon/exchange.png') }}" alt="">
                                    </div>
+<<<<<<< HEAD
                                    <div class="col-9 col-sm-12">
+=======
+                                   <div class="col-9 col-sm-12 brand-icon-text">
+>>>>>>> shadow
                                        <h3>Free 30 Days Returns</h3>
                                    </div>
                                </div>
                             </div>
-                          
-                            <div class="col-12 col-sm-6">  
+
+                            <div class="col-12 col-sm-6">
                                <div class="row">
                                    <div class="col-3 col-sm-12">
                                    <img class="brands-icon" src="{{ asset('/images/icon/guarantee.png') }}" alt="">
                                    </div>
+<<<<<<< HEAD
                                    <div class="col-9 col-sm-12">
+=======
+                                   <div class="col-9 col-sm-12 brand-icon-text">
+>>>>>>> shadow
                                        <h3>10 years Warranty</h3>
                                    </div>
                                </div>
-                            </div>                       
+                            </div>
                         </div>
                     </div>
-                </div>            
+                </div>
             </div>
         </div>
-    </div>  
+    </div>
+<<<<<<< HEAD
     <hr class="brands-hr">
+=======
+
+>>>>>>> shadow
     <div class="brands-prod-container">
         <div class="container">
             <div id="product-div-main" class="row"></div>
@@ -89,14 +109,15 @@
                     <p>Mirrored Silver Chest & Dresser Bedroom Set</p>
                 </div>
             </div>
-            
+
            </div>
         </div> -->
-    </div>          
+    </div>
     @include('./partials/brandassociation')
+<<<<<<< HEAD
             <!-- </div>
-        </div>    
-        
+        </div>
+
     </div> -->
     <!-- @include('./partials/brandassociation')  -->
 @endsection
@@ -105,4 +126,13 @@
     <script src="{{ mix('js/brands.js')}}"></script>
  <script src="{{ mix('js/detailOverview.js')}}"></script> -->
 @endpush -->
+=======
+
+@endsection
+    <script src="{{ ('./js/apis/brands-api.js')}}"></script>
+    @push('pageSpecificScripts')
+    <script src="{{ mix('js/brands.js')}}"></script>
+ <script src="{{ mix('js/detailOverview.js')}}"></script>
+ @endpush
+>>>>>>> shadow
 
