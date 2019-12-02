4060819f078df4c4e3ea7861595a215546eb1fda
Himanshu <emailfromhimanshu@gmail.com>
2019-12-02 12:46:45 +0530

    hotfix

--- resources/views/partials/brandassociation.blade.php
+++ resources/views/partials/brandassociation.blade.php
@@ -8,25 +8,24 @@
             </div>
             <div class="row">
                 <div class="col-md-2 col-sm-4">
-                        <img class="brand-logo -cb2" src="{{ asset('/images/brand-images/cb2.png') }}" alt="CB2 brand">
-                    </div>
-                    <div class="col-md-2 col-sm-4">
-                        <img class="brand-logo -crate" src="{{ asset('/images/brand-images/crate-barrel.png') }}" alt="Crate and barrel brand">
-                    </div>
-                    <div col-md-2 col-sm-4>
-                        <img class="brand-logo -floyd" src="{{ asset('/images/brand-images/Floyd.png') }}" alt="and more coming soon">
-                    </div>
-                    <div class="col-md-2 col-sm-4">
-                        <img class="brand-logo -pier1" src="{{ asset('/images/brand-images/pier1.png') }}" alt="Pier1 brand">
-                    </div>
-                    <div class="col-md-2 col-sm-4">
-                        <img class="brand-logo -westelm" src="{{ asset('/images/brand-images/westelm.png') }}" alt="WestElm brand">
-                    </div>
-                    <div class="col-md-2 col-sm-4">
-                        <img class="brand-logo -worldmarket" src="{{ asset('/images/brand-images/worldmarket.png') }}" alt="World Market brand">
-                    </div>
-
+                    <img class="brand-logo -cb2" src="{{ asset('/images/brand-images/cb2.png') }}" alt="CB2 brand">
                 </div>
+                <div class="col-md-2 col-sm-4">
+                    <img class="brand-logo -crate" src="{{ asset('/images/brand-images/crate-barrel.png') }}" alt="Crate and barrel brand">
+                </div>
+                <div col-md-2 col-sm-4>
+                    <img class="brand-logo -floyd" src="{{ asset('/images/brand-images/Floyd.png') }}" alt="and more coming soon">
+                </div>
+                <div class="col-md-2 col-sm-4">
+                    <img class="brand-logo -pier1" src="{{ asset('/images/brand-images/pier1.png') }}" alt="Pier1 brand">
+                </div>
+                <div class="col-md-2 col-sm-4">
+                    <img class="brand-logo -westelm" src="{{ asset('/images/brand-images/westelm.png') }}" alt="WestElm brand">
+                </div>
+                <div class="col-md-2 col-sm-4">
+                    <img class="brand-logo -worldmarket" src="{{ asset('/images/brand-images/worldmarket.png') }}" alt="World Market brand">
+                </div>
+
             </div>
             <div class="socials col-md-2 px-0 d-none d-md-block">
                 <div class="-text">Connect with us</div>
