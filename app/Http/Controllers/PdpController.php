73de92e2f5ec88b69a5917c49b1970a28cb12fc5
Aditya <adityasaxena602@gmail.com>
2019-12-03 11:59:18 +0530

    adding rediret on desktop to catgeory page

--- app/Http/Controllers/PdpController.php
+++ app/Http/Controllers/PdpController.php
@@ -5,14 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\Department;
 use App\Models\DepartmentMapping;
 use App\Models\Product;
+use App\Models\Utility;
 use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class PdpController extends Controller
 {
-    public function index(Request $request)
+    public function index($sku, Request $request)
     {
-        return view('pages.detailspage');
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        
+        if (Utility::is_mobile($agent)) {
+            return view('pages.detailspage');
+        }
+        else {
+            $product_LS_ID = Product::get_product_LS_ID($sku);
+            if ($product_LS_ID != null) {
+                $depts = Department::get_department_info($product_LS_ID);
+                if (sizeof($depts) > 0) {
+                    $redirect_url = $depts[0]['category_url'] . "?model_sku=" . $sku;
+                    return redirect($redirect_url);
+                }
+            }
+        }
+        
     }
 
 }
