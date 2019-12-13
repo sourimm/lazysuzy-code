75a8fe088922d21e05413a612c588208c9205f9b
Himanshu <emailfromhimanshu@gmail.com>
2019-12-13 16:08:47 +0530

    footer fixes

--- resources/views/partials/brandassociation.blade.php
+++ resources/views/partials/brandassociation.blade.php
@@ -6,37 +6,37 @@
                 <span>Your source for latest styles and deals across top brands.
                 </span>
             </div>
-            <div class="row">
-                <div class="col-md-2 col-sm-4">
+            <div class="brand-row">
+                <div>
                     <a href="/brand/cb2">
                         <img class="brand-logo -cb2" src="{{ asset('/images/brand-images/cb2.png') }}" alt="CB2 brand">
                     </a>
                 </div>
-                <div class="col-md-2 col-sm-4">
+                <div>
                     <a href="/brand/cab">
                         <img class="brand-logo -crate" src="{{ asset('/images/brand-images/crate-barrel.png') }}"
                             alt="Crate and barrel brand">
                     </a>
                 </div>
-                <div class="col-md-2 col-sm-4">
+                <div>
                     <a href="/brand/floyd">
                         <img class="brand-logo -floyd" src="{{ asset('/images/brand-images/Floyd.png') }}"
                             alt="and more coming soon">
                     </a>
                 </div>
-                <div class="col-md-2 col-sm-4">
+                <div>
                     <a href="/brand/pier1">
                         <img class="brand-logo -pier1" src="{{ asset('/images/brand-images/pier1.png') }}"
                             alt="Pier1 brand">
                     </a>
                 </div>
-                <div class="col-md-2 col-sm-4">
+                <div>
                     <a href="/brand/westelm">
                         <img class="brand-logo -westelm" src="{{ asset('/images/brand-images/westelm.png') }}"
                             alt="WestElm brand">
                     </a>
                 </div>
-                <div class="col-md-2 col-sm-4">
+                <div>
                     <a href="/brand/nw">
                         <img class="brand-logo -worldmarket" src="{{ asset('/images/brand-images/worldmarket.png') }}"
                             alt="World Market brand">
