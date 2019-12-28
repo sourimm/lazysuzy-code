5b6a80f7d93858411d5371f7e9866ce674abc38a
Aditya <adityasaxena602@gmail.com>
2019-12-28 15:04:11 +0530

    adding view board catgeory API

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+
 
 class DepartmentController extends Controller
 {
     public function index(Request $request)
     {
+        $is_board_view = INPUT::get("board-view");
+        if ($is_board_view === "true") 
+            return response()->json(Department::get_board_categories());
+
         return response()->json(Department::get_all_Departments());
     }
 
