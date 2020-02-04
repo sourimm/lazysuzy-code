c2252c2196fab72e00e6d90411609fc53b6c56e9
Himanshu Jain <emailfromhimanshu@gmail.com>
2020-02-04 17:24:36 +0530

    issues fixed

--- resources/views/pages/detailspage.blade.php
+++ resources/views/pages/detailspage.blade.php
@@ -39,9 +39,68 @@
                         <i class="fas fa-arrow-right"></i>
                     </button>    -->
                 </div>
-               
+                <p class="-desc d-none d-md-block"></p>
+                <div class="d-none d-md-block">
+                    <div class="accordion" id="accordionExample">
+                        <div class="card d-none">
+                            <!-- <div class="card-header" id="headingOne"> -->
+                            <div id="headingOne">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse"
+                                        data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <span class="-text">Description</span>
+                                    </button>
+                                </h2>
+                            </div>
 
-                <div class="d-md-block d-lg-none">
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <p class="-desc"></p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- <div class="card"> -->
+                        <div>
+                            <!-- <div class="card-header" id="headingTwo"> -->
+                            <div id='headingTwo'>
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link  collapsed" type="button" data-toggle="collapse"
+                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <span class="-text">▼ Features </span>
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                                data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <div class="-features"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <!-- <div class="card"> -->
+                            <!-- <div class="card-header" id="headingThree"> -->
+                            <div id="headingThree">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                        data-target="#collapseThree" aria-expanded="false"
+                                        aria-controls="collapseThree">
+                                        <span class="-text">▼ Dimensions</span>
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                                data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <div class="-dimension row"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-sm-block d-md-none">
                     <!-- Nav tabs -->
                     <ul class="nav nav-tabs">
                         <li class="nav-item">
@@ -66,6 +125,8 @@
 
         </div>
 
+        <div class="prod-price-card col-3 d-none">
+        </div>
     </div>
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-lightbox/0.2.12/slick-lightbox.css">
