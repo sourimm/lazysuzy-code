10d9b43a56644be5c97c00b7f90245a1793feeaf
Aditya <adityasaxena602@gmail.com>
2020-02-07 20:16:28 +0530

    adding seo data to listing API

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -686,7 +686,7 @@ class Product extends Model
                 "page_title" => $d->cat_name_long,
                 "full_title" => $d->dept_name_long . " "  . $d->cat_name_short,
                 "email_title" => $d->dept_name_long . " " . $d->cat_name_short,
-                "description" => "Search hundreds of " . $d->cat_name_long  . "from top brands at once. Add to your room designs with your own design boards." 
+                "description" => "Search hundreds of " . $d->cat_name_long  . " from top brands at once. Add to your room designs with your own design boards." 
             ];
         }
         return [
