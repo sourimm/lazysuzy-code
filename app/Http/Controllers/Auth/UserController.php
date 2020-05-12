762f6b1f6fbcafa3d1343e0636b57cf735aa5103
Aditya <adityasaxena602@gmail.com>
2020-05-12 23:56:27 +0530

    adding user update, changes logic

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -197,72 +197,85 @@ class UserController extends Controller
         $user = Auth::user();
         return response()->json(['success' => $user], $this->successStatus);
     }
-    
-    public function update(Request $request){
-      $data = $request->all();
-      if(Auth::check()){
-        $user = Auth::user();
-        
-        if(isset($data['name'])){
-          $name = explode(' ', $data['name']);
-          $user->first_name = isset($name[0]) ? $name[0] : '';
-          $user->last_name = isset($name[1]) ? implode(' ', array_slice($name, 1)) : '';
-          $user->name = $data['name'];
-        }
-        
-        if(isset($data['email']) && !isset($data['password'])){
-          $validator = Validator::make($data, ['email' => ['required', 'string', 'email', 'max:255', 'unique:users']]);
-          
-          if ($validator->fails())
-            return response()->json(['error' => $validator->errors()], 401);
-          
-          $user->email = $data['email'];
-        }
-        
-        // set the password only if does not already exists
-        if(isset($data['password']) && empty($user->password) && $user->user_type == config('user.user_type.guest')){
-          $validator = Validator::make($data, ['password' => ['required', 'string', 'min:8']]);
 
-          $validator = Validator::make($data, [
-            'password' => ['required', 'string', 'min:8'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
-          ]);
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        if (Auth::check()) {
+            $user = Auth::user();
 
-            if ($validator->fails()) {
-                if (isset($data['password'])) {
-                    $creds = $request->only('email', 'password');
-                    if (Auth::guard('web')->attempt(['email' => $data['email'], 
-                            'password' => $data['password']], false, false)) {
-
-                        // change the userID in order table to new user's userID here.
-                        $user = Auth::guard('web')->user();
-                        $success['token'] =  $user->createToken('Laravel Personal Access Client')->accessToken;
-                        return response()->json([
-                            'success' => $success,
-                            'user' => $user
-                        ], 200);
-                    }
-                    else {
-                        return response()->json(['error' => 'Unauthorised'], 401);
-                    }
+            if (isset($data['name'])) {
+                $name = explode(' ', $data['name']);
+                $user->first_name = isset($name[0]) ? $name[0] : '';
+                $user->last_name = isset($name[1]) ? implode(' ', array_slice($name, 1)) : '';
+                $user->name = $data['name'];
             }
-            return response()->json(['error' => $validator->errors()], 401);
-        }
 
-            $user->password = Hash::make($data['password']);
+            if (isset($data['email']) && !isset($data['password'])) {
+                $validator = Validator::make($data, ['email' => ['required', 'string', 'email', 'max:255', 'unique:users']]);
+
+                if ($validator->fails())
+                    return response()->json(['error' => $validator->errors()], 401);
+
+                $user->email = $data['email'];
+                $success['token'] =  $user->createToken('lazysuzy-web')->accessToken;
+                return response()->json(['success' => $success, 'user' => $user], $this->successStatus);
+            }
+
+            // set the password only if does not already exists
+            if (isset($data['password']) && empty($user->password) && $user->user_type == config('user.user_type.guest')) {
+                $validator = Validator::make($data, ['password' => ['required', 'string', 'min:8']]);
+
+                $validator = Validator::make($data, [
+                    'password' => ['required', 'string', 'min:8'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
+                ]);
+
+                if ($validator->fails()) {
+
+                    $failedRules = $validator->failed();
+
+                    if (isset($failedRules['email']['Unique'])) {
+
+
+                        if (isset($data['password'])) {
+
+                            $creds = $request->only('email', 'password');
+                            if (Auth::guard('web')->attempt([
+                                'email' => $data['email'],
+                                'password' => $data['password']
+                            ], false, false)) {
+
+                                // change the userID in order table to new user's userID here.
+                                $user = Auth::guard('web')->user();
+                                $success['token'] =  $user->createToken('Laravel Personal Access Client')->accessToken;
+                                return response()->json([
+                                    'success' => $success,
+                                    'user' => $user
+                                ], 200);
+                            } else {
+                                return response()->json(['error' => 'Unauthorised'], 401);
+                            }
+                        }
+                    }
+                    return response()->json(['error' => $validator->errors()], 401);
+                }
+
+                $user->password = Hash::make($data['password']);
+                $user->email = $data['email'];
+            }
+
+            // if both email and password exist for user change its type
+            if (!empty($user->email) && !empty($user->password) && $user->user_type == config('user.user_type.guest'))
+                $user->user_type = config('user.user_type.default');
+
+            if ($user->update()) {
+                $success['token'] =  $user->createToken('lazysuzy-web')->accessToken;
+
+                return response()->json(['success' => $success, 'user' => $user], $this->successStatus);
+            } else
+                return response()->json(['error' => ['error' => "unknown error"]], 401);
         }
-        
-        // if both email and password exist for user change its type
-        if(!empty($user->email) && !empty($user->password) && $user->user_type == config('user.user_type.guest'))
-          $user->user_type = config('user.user_type.default');
-          
-        if($user->update()) {
-          $success['token'] =  $user->createToken('lazysuzy-web')->accessToken;
-          $success['user'] =  $user;
-          return response()->json(['success' => $success], $this->successStatus);
-        }
-        else
-          return response()->json(['error' => ['error' => "unknown error"]], 401);
-      }
     }
+
 }
