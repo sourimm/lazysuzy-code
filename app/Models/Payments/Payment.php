fbaa689493c6e41f16ea5e8a7afbda468157b4bb
Aditya <adityasaxena602@gmail.com>
2020-05-13 11:43:15 +0530

    adding support for shipping and card details in receipt mailer

--- app/Models/Payments/Payment.php
+++ app/Models/Payments/Payment.php
@@ -19,13 +19,14 @@ class Payment extends Model
     private static $shipment_code_table = 'lz_ship_code';
     private static $failed_reciepts = 'lz_failed_receipt';
 
-    public static function charge($req) {
+    public static function charge($req)
+    {
         $user_id = Auth::check() ? Auth::user()->id : 'guest-1';
         $username = Auth::check() ? Auth::user()->first_name : $req->input('billing_f_Name');
         $mail_data = [];
         $total_price = 0;
         $total_items = 0;
-        $shipment_cost = 1;
+        $shipment_cost = 0;
         $mail_data['order'] = [];
         $mail_data['order']['products'] = [];
 
@@ -38,24 +39,24 @@ class Payment extends Model
         $collisions = 0;
         $order_id = strtoupper(Str::random($length));
         $found = DB::table(Payment::$order_table)
-                    ->where('order_id', $order_id)
-                    ->count();
+            ->where('order_id', $order_id)
+            ->count();
 
-        while($found) {
+        while ($found) {
             $order_id = strtoupper(Str::random($length));
             $found = DB::table(Payment::$order_table)
                 ->where('order_id', $order_id)
                 ->count();
             $collisions++;
 
-            if($collisions > 100) {
+            if ($collisions > 100) {
                 $length += 1;
-                $collisions = 0; 
+                $collisions = 0;
             }
         }
 
         //$order_id = "lz-ord-" . rand(1, 1000) . "-" . rand(1, 10000);
-        foreach($cart as $product) {
+        foreach ($cart as $product) {
 
             // add mail reciept data
             $mail_data['order']['products'][] = [
@@ -74,17 +75,17 @@ class Payment extends Model
             $ship_code = strtolower($product->ship_code);
 
             // if not free-shipping
-            if($ship_code != 'f')
-                $shipment_cost *= (float)($product->ship_custom * $product->count);
+            if ($ship_code != 'f')
+                $shipment_cost += (float) ($product->ship_custom * $product->count);
 
             $row = DB::table('lz_inventory')
                 ->select('quantity')
                 ->where('product_sku', $product->product_sku)
                 ->get();
 
-            if(isset($row[0])) {
+            if (isset($row[0])) {
                 $available_count = $row[0]->quantity;
-                if($available_count < $product->count) {
+                if ($available_count < $product->count) {
                     return [
                         'status' => 'failed',
                         'msg' => 'Product ' . $product->product_name . ' not available.'
@@ -98,27 +99,25 @@ class Payment extends Model
                         'price' => (float) $product->retail_price,
                         'quantity' => $product->count
                     ]);
-            }
-            else {
+            } else {
                 return [
                     'status' => 'failed',
                     'msg' => 'Product ' . $product->name . ' not in inventory.'
                 ];
             }
-           
         }
-        
+
         $mail_data['total_price'] = '$' . $total_price;
 
         // if cost of shipment is 1 then set it to 0
         // 1 is from when the variable was declared, 
         // cart has all Free Shipment products 
-        if($shipment_cost == 1) {
+        if ($shipment_cost == 1) {
             $shipment_cost = 0;
         }
 
-        $total_price += $shipment_cost; 
-        $mail_data['shipping'] = '$' . ($total_items * 25);
+        $total_price += $shipment_cost;
+        $mail_data['shipping'] = '$' . $shipment_cost;
         $mail_data['order_cost'] = '$' . $total_price;
 
         Stripe\Stripe::setApiKey(env('STRIP_SECRET'));
@@ -162,8 +161,8 @@ class Payment extends Model
                     'country' => substr($req->input('billing_country'), 0, 2),
                     'postal_code' => $req->input('shipping_zipcode'),
                 ],
-            'name' => $req->input('shipping_f_Name') . ' ' . $req->input('shipping_l_Name'),
-            'phone' => $req->input('shipping_phone')
+                'name' => $req->input('shipping_f_Name') . ' ' . $req->input('shipping_l_Name'),
+                'phone' => $req->input('shipping_phone')
             ]
 
         ]);
@@ -174,20 +173,20 @@ class Payment extends Model
                 'stripe_transaction_id' => $charge->id,
                 'status' => $charge->status
             ]);
-        
-        if($charge->status == 'succeeded') {
+
+        if ($charge->status == 'succeeded') {
             // remove from stock;
-            foreach($cart as $product) {
+            foreach ($cart as $product) {
                 $p = $product;
                 $in_stock = DB::table('lz_inventory')
-                                ->select("quantity")
-                                ->where("product_sku", $p->product_sku)
-                                ->get();
+                    ->select("quantity")
+                    ->where("product_sku", $p->product_sku)
+                    ->get();
 
-                if(isset($in_stock[0])) {
+                if (isset($in_stock[0])) {
                     $original_count = $in_stock[0]->quantity;
                     $left_count = $original_count - $p->count;
-                    
+
                     Cart::remove($p->product_sku, $p->count);
 
                     DB::table('lz_inventory')
@@ -204,19 +203,27 @@ class Payment extends Model
             $ID = DB::table('lz_order_delivery')
                 ->insertGetId($delivery_details);
             $customer_name = $req->input('billing_f_Name');
+
+            // get card details for sending in email reciept
+            $card = Stripe\Token::retrieve($req->input('token'));
+            $mail_data['card'] = [
+                'last4' => $card->card->last4,
+                'expiry' => $card->card->exp_month . '/' . $card->card->exp_year
+            ];
+            $mail_data['shipping_details'] = $delivery_details;
+            $mail_data['order_id'] = $order_id;
             $receipt_send = Mailer::send_receipt($req->input('email'), $customer_name, $mail_data);
-            
-            if(!$receipt_send['status']) {
+
+            if (!$receipt_send['status']) {
                 // save this order in DB to send the 
                 // mail later
                 DB::table(Payment::$failed_reciepts)
-                        ->insert([
-                            'email' => $req->input('email'),
-                            'mail_data' => json_encode($mail_data),
-                            'error' => $receipt_send['error']
-                        ]);
+                    ->insert([
+                        'email' => $req->input('email'),
+                        'mail_data' => json_encode($mail_data),
+                        'error' => $receipt_send['error']
+                    ]);
             }
-            
         }
 
         return [
@@ -227,11 +234,14 @@ class Payment extends Model
             'transaction_id' => isset($charge->id) ? $charge->id : null,
             'reciept_url' => isset($charge->receipt_url) ? $charge->receipt_url : null,
             'order' => $req->all(),
-            'receipt_send' => $receipt_send
+            'receipt_send' => $receipt_send,
+            'mail_data' => $mail_data,
+            'card' => $card
         ];
     }
 
-    public static function order($order_id) {
+    public static function order($order_id)
+    {
         $response = [];
         $user = Auth::user();
         $rows_shipment_code = DB::table(Payment::$shipment_code_table)
@@ -239,27 +249,27 @@ class Payment extends Model
             ->toArray();
         $shipment_codes = array_column($rows_shipment_code, 'rate', 'code');
         $response['cart'] = DB::table('lz_orders')
-                        ->select(
-                            Payment::$order_table . '.product_sku',
-                            Payment::$order_table . '.price',
-                            Payment::$order_table . '.quantity as count',
-                            'master_data.product_name',
-                            'master_data.was_price',
-                            DB::raw('concat("https://www.lazysuzy.com", master_data.main_product_images) as image'),
-                            'master_data.product_description',
-                            'master_data.reviews',
-                            'master_data.rating',
-                            'master_brands.name as site',
-                            'lz_inventory.ship_custom',
-                            'lz_inventory.ship_code'
-                        )
-                        ->join("master_data", "master_data.product_sku", "=", Payment::$order_table . ".product_sku")
-                        ->join("lz_inventory", "lz_inventory.product_sku", "=", "master_data.product_sku")
-                        ->join("master_brands", "master_data.site_name", "=", "master_brands.value")
-                        ->where('order_id', $order_id)
-                        ->where('user_id', $user->id)
-                        ->get();
-        
+            ->select(
+                Payment::$order_table . '.product_sku',
+                Payment::$order_table . '.price',
+                Payment::$order_table . '.quantity as count',
+                'master_data.product_name',
+                'master_data.was_price',
+                DB::raw('concat("https://www.lazysuzy.com", master_data.main_product_images) as image'),
+                'master_data.product_description',
+                'master_data.reviews',
+                'master_data.rating',
+                'master_brands.name as site',
+                'lz_inventory.ship_custom',
+                'lz_inventory.ship_code'
+            )
+            ->join("master_data", "master_data.product_sku", "=", Payment::$order_table . ".product_sku")
+            ->join("lz_inventory", "lz_inventory.product_sku", "=", "master_data.product_sku")
+            ->join("master_brands", "master_data.site_name", "=", "master_brands.value")
+            ->where('order_id', $order_id)
+            ->where('user_id', $user->id)
+            ->get();
+
 
         foreach ($response['cart'] as $product) {
             // set correct shipping cost
@@ -269,18 +279,18 @@ class Payment extends Model
                 $product->ship_custom = 0;
             }
         }
-        
+
         $response['delivery'] = DB::table(Payment::$delivery_table)
-                                ->where('order_id', $order_id)
-                                ->where('user_id', $user->id)
-                                ->get();
+            ->where('order_id', $order_id)
+            ->where('user_id', $user->id)
+            ->get();
 
         $response['payment'] = [];
-        if(isset($response['delivery'][0])) {
+        if (isset($response['delivery'][0])) {
             Stripe\Stripe::setApiKey(env('STRIP_SECRET'));
             $response['payment'] = Stripe\Token::retrieve($response['delivery'][0]->token);
         }
-      
+
         return $response;
     }
 }
