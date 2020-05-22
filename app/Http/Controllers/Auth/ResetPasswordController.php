8bdc6ae50c5abe7e137405939815a332401b13bd
Aditya <adityasaxena602@gmail.com>
2020-05-23 01:16:34 +0530

    adding redirect route after pass reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = 'https://www.lazysuzy.com';
 
     /**
      * Create a new controller instance.
