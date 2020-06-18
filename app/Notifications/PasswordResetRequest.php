a965308f4adc4435e5c2a467d549a654b6936cee
Aditya <adityasaxena602@gmail.com>
2020-06-18 20:53:01 +0530

    change password reset link

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -42,10 +42,10 @@ class PasswordResetRequest extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = url('/reset-password/' . $this->token);
+        $url = config('app.password_reset') . '/reset-password/' . $this->token;
         return (new MailMessage)
                     ->line('You are receiving this email because we received a password reset request for your account.')
-                    ->action('Reset Password', url($url))
+                    ->action('Reset Password', $url)
                     ->line('If you did not request a password reset, no further action is required.');
     }
 
