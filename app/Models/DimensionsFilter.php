e71a5bd1acc1ebfe7696b2b1b7332443f66cf83c
Aditya <adityasaxena602@gmail.com>
2020-10-27 20:48:03 +0530

    fix function bug from size filter

--- app/Models/DimensionsFilter.php
+++ app/Models/DimensionsFilter.php
@@ -91,7 +91,7 @@ class DimensionsFilter extends Model
             }
         }
 
-        $products = CollectionFilter::apply_dimensions_filters($products, $all_filters);
+        $products = CollectionFilter::apply_collections_filter($products, $all_filters);
         // get all min and max values for all dimensions columns
         foreach($dim_columns as $column) {
             //$products = $products->where($column, '>', 0);
