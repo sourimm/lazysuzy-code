3a8bbd568154a613677a0367c2102630b36e1809
Aditya <adityasaxena602@gmail.com>
2020-11-09 17:13:59 +0530

    [Hot Fix] change mfg_country label to country

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -190,7 +190,7 @@ class Product extends Model
 
                     // change filter keys for some filters 
                     $block_str[0] = ($block_str[0] == "country") ? "mfg_country" : $block_str[0];
-                    
+
                     $all_filters[$block_str[0]] = explode(",", $block_str[1]);
                     $all_filters[$block_str[0]] = array_map("strtolower", $all_filters[$block_str[0]]);
                 }
@@ -1347,7 +1347,7 @@ class Product extends Model
             "material" => MaterialFilter::get_filter_data($dept, $cat, $all_filters),
             "fabric" => FabricFilter::get_filter_data($dept, $cat, $all_filters),
             "designer" => DesignerFilter::get_filter_data($dept, $cat, $all_filters),
-            "mfg_country" => MFDCountry::get_filter_data($dept, $cat, $all_filters),
+            "country" => MFDCountry::get_filter_data($dept, $cat, $all_filters),
         ];
         
         //$dept, $cat, $subCat
