9d4dce9e8a4f9aa66de1a52950744dfc3c2b2ace
Aditya <adityasaxena602@gmail.com>
2020-11-09 17:07:14 +0530

    [Hot Fix] change mfg_country to country in filters

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -187,6 +187,10 @@ class Product extends Model
                 $block_str = explode(":", $block);
 
                 if (isset($block_str[0]) && isset($block_str[1])) {
+
+                    // change filter keys for some filters 
+                    $block_str[0] = ($block_str[0] == "country") ? "mfg_country" : $block_str[0];
+                    
                     $all_filters[$block_str[0]] = explode(",", $block_str[1]);
                     $all_filters[$block_str[0]] = array_map("strtolower", $all_filters[$block_str[0]]);
                 }
