ca2f465c2224180669d4f7ec1f6d9737f9aea1a5
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-19 00:53:50 +0530

    Update API.php

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -261,7 +261,7 @@ class API extends Controller
             }*/
 
 
-            return Review::save_product_review(Request $request,$data, $id);
+            return Review::save_product_review($request,$data, $id);
             
     }
 	
