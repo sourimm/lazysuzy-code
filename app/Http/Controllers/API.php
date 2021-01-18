31cb15f8e8a3cfa0105ccc160a7e692d16c02e9f
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-19 01:17:28 +0530

    Update API.php

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -252,16 +252,8 @@ class API extends Controller
         $data = $request->all();
         $id= Auth::check() ? Auth::user()->id : 0;
 		 
-           /* if (isset($data['user_email'])) {
-                $validator = Validator::make($data, ['user_email' => ['required', 'string', 'user_email', 'max:255']]);
-
-                if ($validator->fails())
-                    return response()->json(['error' => $validator->errors()], 401);
-
-               
-            }*/
-			
-			if(array_key_exists('rimage', $data) && isset($data['rimage'])) {
+         
+			/*if(array_key_exists('rimage', $data) && isset($data['rimage'])) {
 			
 			$upload_folder = public_path('uimg');
 			for($i=0;$i<count($request->rimage);$i++){
@@ -276,6 +268,17 @@ class API extends Controller
 			}
 			else 
 				$error[] = response()->json(['error' => 'image could not be uploaded. Please try again.'], 422);
+		}*/
+		
+		if ($request->hasFile('rimage')) {
+			$destinationPath = public_path('uimg');
+			$files = $request->file('rimage'); // will get all files
+
+			foreach ($files as $file) {//this statement will loop through all files.
+				$file_name = $file->getClientOriginalName(); //Get file original name
+				echo $file_name;
+				$file->move($destinationPath , $file_name); // move files to destination folder
+			}
 		}
 
 
