cc3be73d3e668b5e10cc8f82988be794fb0e88fa
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-26 14:42:39 +0530

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -41,7 +41,7 @@ class Review extends Model
 						$error[] = response()->json(['error' => 'image could not be uploaded. Please try again.'], 422);
 				//}
 		}
-		// $datetime = date("F j, Y");echo $datetime;
+		 $datetime = date("F j, Y");
 		
 		 $is_inserted = DB::table('master_reviews')
                     ->insert([
