166bd51ff705205487846a6c0fd3ba2be2192c4a
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-29 00:49:08 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2434,7 +2434,7 @@ class Product extends Model
 			
 		 	$product_rows = DB::table('master_reviews')
             ->select('product_sku')
-            ->whereIn('product_sku', $sku_array)
+            ->whereIn('product_sku',["511","1093","1097","1105","1131"]) //$sku_array
 			->where('product_sku', '!=', $sku)
             ->get();
 			
@@ -2449,6 +2449,6 @@ class Product extends Model
 		}
 		
 
-        return $sku_array;
+        return $response_product;
     }
 };
