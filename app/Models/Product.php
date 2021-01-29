dd6629c082fd393fa6c63749ed04d88144a33907
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-29 12:35:08 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2464,7 +2464,7 @@ class Product extends Model
 				 
 
 				foreach ($product_rows as $product) {
-					$LS_ID_array = explode(","$product->LS_ID);
+					$LS_ID_array = explode(",",$product->LS_ID);
 					if(in_array(0, $LS_ID_array))
 						array_push($response,$product);
 				}
