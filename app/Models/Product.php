b5eee11bf00b3010b6bb5e01d429ea7a8e4befe5
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-30 00:12:43 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2619,7 +2619,7 @@ $flag = 0;
 			$LS_ID_arr = explode(",",$pr->LS_ID);
 			//$flag = in_array($LSID, $LS_ID_arr);
 			//if(in_array($LSID, $LS_ID_arr)){
-			if( $LSID==$LS_ID_arr[0]){	
+			if( strcmp($LSID,$pr->LS_ID)){	
 				array_push($response_identical,$pr);
 			}
 			else{
