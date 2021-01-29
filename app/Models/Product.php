74bd22f2c33af076b7628f62f852e80513681927
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-01-30 00:10:20 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2615,11 +2615,11 @@ class Product extends Model
 		$response_identical = [];
 		$remainarr = [];
 $flag = 0;
-		foreach($product_rows as $pr){
-			$pr->LS_ID = '1123';	
+		foreach($product_rows as $pr){ 
 			$LS_ID_arr = explode(",",$pr->LS_ID);
-			$flag = in_array($LSID, $LS_ID_arr);
-			if(in_array($LSID, $LS_ID_arr)){$flag++;
+			//$flag = in_array($LSID, $LS_ID_arr);
+			//if(in_array($LSID, $LS_ID_arr)){
+			if( $LSID==$LS_ID_arr[0]){	
 				array_push($response_identical,$pr);
 			}
 			else{
@@ -2627,7 +2627,7 @@ $flag = 0;
 			}
 			
 		}
-		return $flag;
+		return $response_identical;
 		$LSID_dept = $LSID[0].$LSID[1].$LSID[2];
 		
 		
