6e5bba40794f5d2ff7d2a3dfad6e2f6b617d83d1
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-01 21:48:28 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2462,7 +2462,8 @@ class Product extends Model
 				->whereIn('master_data.product_sku', $sku_array)  
 				->where('master_data.product_status','active') 
 				->join('user_views', 'user_views.product_sku', '=', 'master_data.product_sku')	
-				->select(['master_data.id','master_data.serial','master_data.product_status','master_data.product_name','master_data.product_sku','master_data.brand','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.updated_at','user_views.num_views'])
+				->join('master_brands', 'master_brands.value', '=', 'master_data.brand')		
+				->select(['master_data.id','master_data.product_description','master_data.product_status','master_data.product_name','master_data.product_sku','master_brands.name','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.updated_at as last_visit','user_views.num_views as visit_count'])
 				->get();
 			
 			    if(strlen($LSID)==3){
@@ -2585,8 +2586,9 @@ class Product extends Model
 					
 					$product_rows1 = DB::table('user_views') 
 					->whereIn('user_views.product_sku', $sku_array)  
-					->join('master_data', 'user_views.product_sku', '=', 'master_data.product_sku')					
-					->select(array('master_data.id','master_data.serial','master_data.product_status','master_data.product_name','master_data.product_sku','master_data.brand','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.product_sku','user_views.updated_at','user_views.num_views'))
+					->join('master_data', 'user_views.product_sku', '=', 'master_data.product_sku')		
+					->join('master_brands', 'master_brands.value', '=', 'master_data.brand')						
+					->select(array('master_data.id','master_data.product_description','master_data.product_status','master_data.product_name','master_data.product_sku','master_brands.name','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.product_sku','user_views.updated_at as last_visit','user_views.num_views as visit_count'))
 					->groupBy('user_views.product_sku')
 					->orderBy(\DB::raw('count(user_views.user_id)'), 'DESC')
 					->get();
@@ -2730,8 +2732,9 @@ class Product extends Model
 					
 					$product_rows1 = DB::table('user_views') 
 					->whereIn('user_views.product_sku', $sku_array)  
-					->join('master_data', 'user_views.product_sku', '=', 'master_data.product_sku')					
-					->select(array('master_data.id','master_data.serial','master_data.product_status','master_data.product_name','master_data.product_sku','master_data.brand','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.product_sku','user_views.updated_at','user_views.num_views'))
+					->join('master_data', 'user_views.product_sku', '=', 'master_data.product_sku')	
+					->join('master_brands', 'master_brands.value', '=', 'master_data.brand')						
+					->select(array('master_data.id','master_data.product_description','master_data.product_status','master_data.product_name','master_data.product_sku','master_brands.name','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','user_views.product_sku','user_views.updated_at as last_visit','user_views.num_views as visit_count'))
 					->groupBy('user_views.product_sku')
 					->orderBy(\DB::raw('count(user_views.user_id)'), 'DESC')
 					->get();
