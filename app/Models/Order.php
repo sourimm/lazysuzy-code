49f4c528378eb86c487b5032196ac16a49249c6e
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-04 17:22:13 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -42,14 +42,14 @@ class Order extends Model
 			 foreach($product_rows_child as $pr) {  
 				
 					
-				/*for($i = 0; $i < count($product_rows_child); $i++)
+				for($i = 0; $i < count($product_rows_child); $i++)
 				{
 				  if($product_rows_child[$i]->order_id == $pr->order_id)
 				  {
 					array_push($child_array,$product_rows_child[$i]);
 				  }
 				}
-				$pr->orders = 	json_encode($child_array) ;*/
+				$pr->orders = 	json_encode($child_array) ;
 				array_push($head_array,$pr); 
 			}
 			
@@ -67,56 +67,10 @@ class Order extends Model
 	}
 
    
-    public static function get_order_status($user_id)
-    {
-		$head_array = [];
-		$child_array = [];
-		
-		if($user_id>0)
-		{
-			
-			/*$product_rows = DB::table('lz_order_delivery') 
-			->where('user_id', $user_id)   						
-			->select("*")
-			->get();*/
-			
-			$product_rows_child = DB::table('lz_orders') 
-			->where('user_id', $user_id)    						
-			->select("*")
-			->get(); 
-				
-			/*foreach($product_rows as $pr) {  */
-			 foreach($product_rows_child as $pr) {  
-				
-					
-				/*for($i = 0; $i < count($product_rows_child); $i++)
-				{
-				  if($product_rows_child[$i]->order_id == $pr->order_id)
-				  {
-					array_push($child_array,$product_rows_child[$i]);
-				  }
-				}
-				$pr->orders = 	json_encode($child_array) ;*/
-				array_push($head_array,$pr); 
-			}
-			
-			$a['status']=true;
-			$a['response'] = $head_array;
-			
-			
-		}
-		else{
-				$a['status']=false;
-				$a['msg']='Not authenticaticated User.';
-		}	
-
-		return $a;	
-	}
-
+  
 	public static function get_order_status($user_id,$orderid,$zipcode)
 	{
-			$head_array = [];
-			$child_array = [];
+	
 			
 			if($user_id>0)
 			{
@@ -135,38 +89,22 @@ class Order extends Model
 				}
 				 
 				
+				if(isset($data)){
+					$response['header']=$data;
+					$response['status']=true;
 				
-				$product_rows_child = DB::table('lz_orders') 
-				->where('user_id', $user_id)    						
-				->select("*")
-				->get(); 
-					
-				/*foreach($product_rows as $pr) {  */
-				 foreach($product_rows_child as $pr) {  
-					
-						
-					/*for($i = 0; $i < count($product_rows_child); $i++)
-					{
-					  if($product_rows_child[$i]->order_id == $pr->order_id)
-					  {
-						array_push($child_array,$product_rows_child[$i]);
-					  }
-					}
-					$pr->orders = 	json_encode($child_array) ;*/
-					array_push($head_array,$pr); 
+				}
+				else{
+						$response['status']=false;
+						$response['msg']='No data found.';
 				}
 				
-				$a['status']=true;
-				$a['response'] = $head_array;
-				
-				
-			}
-			else{
-					$a['status']=false;
-					$a['msg']='Not authenticaticated User.';
-			}	
-
-			return $a;	
 		}
+		else{
+				$response['status']=false;
+				$response['msg']='Not authenticaticated User.';
+		}
+		
+		return $response;	
 
 };
