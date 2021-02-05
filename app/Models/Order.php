1cefc4999102435be447502a7a034be1037086aa
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-05 15:19:40 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -96,22 +96,15 @@ class Order extends Model
 				$response['header']=$data; 
 				
 				foreach($data as $datasingle){ 
-				/*	$product_rows_child = DB::table('lz_orders') 
+					$product_rows_child = DB::table('lz_orders') 
 					->where('order_id', $datasingle->order_id)    	
 					->join('master_data', 'master_data.product_sku', '=', 'lz_orders.product_sku')		
 					->join('lz_inventory', 'lz_inventory.product_sku', '=', 'lz_orders.product_sku')		
 					->join('lz_ship_code', 'lz_ship_code.code', '=', 'lz_inventory.ship_code')		
 					->join('master_brands', 'master_brands.value', '=', 'master_data.brand')	
 					->select(array('lz_orders.quantity','lz_orders.price','lz_orders.status','lz_orders.note','lz_orders.date','lz_orders.tracking','lz_inventory.ship_code','lz_ship_code.label','master_data.product_name','master_data.main_product_images as image','master_brands.name as brand_name','master_data.product_sku','lz_ship_code.label'))
-					->get(); */
-					
-					$product_rows_child = DB::table('lz_orders') 
-					->where('order_id', $datasingle->order_id) 
-				//->join('lz_inventory', 'lz_inventory.product_sku', '=', 'lz_orders.product_sku')					
-					->join('master_data', 'master_data.product_sku', '=', 'lz_orders.product_sku')	
-					->join('master_brands', 'master_brands.value', '=', 'master_data.brand')						
-					->select(array('lz_orders.quantity','lz_orders.price','lz_orders.status','lz_orders.note','lz_orders.date','lz_orders.tracking','master_data.product_name','master_data.main_product_images as image','master_data.product_sku','master_brands.name as brand_name'))
 					->get(); 
+				
 					
 					foreach($product_rows_child as $pr){
 					 	$pr->image =  env('APP_URL').$pr->image; 
