cdd3c7cc073fc54312b8a15e26c23029267f636f
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-05 16:51:29 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2471,7 +2471,7 @@ class Product extends Model
 				->where('master_data.product_status','active') 
 				->join('user_views', 'user_views.product_sku', '=', 'master_data.product_sku')	
 				->join('master_brands', 'master_brands.value', '=', 'master_data.brand')
-				->select(['master_data.id','master_data.product_description','master_data.product_status','master_data.product_name','master_data.product_sku','master_brands.name as brand_name','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID','\DB::raw('count(user_views.user_id)')'])//,'user_views.updated_at as last_visit','user_views.num_views as visit_count'
+				->select(['master_data.id','master_data.product_description','master_data.product_status','master_data.product_name','master_data.product_sku','master_brands.name as brand_name','master_data.price','master_data.was_price','master_data.main_product_images as image','master_data.LS_ID',DB::raw('count(user_views.user_id) as viewers'])//,'user_views.updated_at as last_visit','user_views.num_views as visit_count'
 				->groupBy('user_views.product_sku')
 				->orderBy(\DB::raw('count(user_views.user_id)'), 'DESC')	
 				->get();
