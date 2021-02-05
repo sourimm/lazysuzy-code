e834e6b1afa41b246e67f2e9a91932c420ea99c7
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-06 00:03:25 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8931,7 +8931,7 @@ are exactly alike.',
 						else{
 								array_push($response_catother,$catdept);
 						}*/
-						return 'jjj='.strpos($LS_ID_arr[$i], $LSID[0].$LSID[1]);
+						return 'jjj='.strpos('552', '55');
 						 if (strpos($LS_ID_arr[$i], $LSID[0].$LSID[1]) !== false){
 							if((strpos($LS_ID_arr[$i],$LSID[0].$LSID[1]))==0){
 								array_push($response_catsame,$catdept);
