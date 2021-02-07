d90eadaf4a31227777abe033ed60502d4da90287
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-07 16:24:28 +0530

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -60,6 +60,8 @@ class Review extends Model
 								'rating' => $data['rating'],
 								'submission_time' => $datetime 
 							]);
+							
+							return 'is_inserted='.$is_inserted;
 
       // sent in the request is updated
       return [
