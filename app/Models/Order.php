cee69bf533f998bdc676fa8c0c1ca8fa392f7cfe
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-08 15:59:27 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -113,7 +113,7 @@ class Order extends Model
 				foreach($data as $datasingle){ 
 				   
 				   $datasingle->created_at = date("F j, Y", strtotime($datasingle->created_at));
-				   array_push($arrheader['header'],$datasingle);
+				   array_push($arrheader,$datasingle);
 					$product_rows_child = DB::table('lz_orders') 
 					->where('order_id', $datasingle->order_id)    	
 					->join('master_data', 'master_data.product_sku', '=', 'lz_orders.product_sku')		
