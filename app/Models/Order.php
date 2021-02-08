4b1fac4ca6d8bdf003a7aaf80e802fd7fd44da6f
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-08 16:21:21 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -113,10 +113,10 @@ class Order extends Model
 					$product_rows_child = DB::table('lz_orders') 
 					->where('order_id', $datasingle->order_id)    	
 					->join('master_data', 'master_data.product_sku', '=', 'lz_orders.product_sku')		
-					->join('lz_inventory', 'lz_inventory.product_sku', '=', 'lz_orders.product_sku')		
-					->join('lz_ship_code', 'lz_ship_code.code', '=', 'lz_inventory.ship_code')		
+					//->join('lz_inventory', 'lz_inventory.product_sku', '=', 'lz_orders.product_sku')		
+					//->join('lz_ship_code', 'lz_ship_code.code', '=', 'lz_inventory.ship_code')		
 					->join('master_brands', 'master_brands.value', '=', 'master_data.brand')	
-					->select(array('lz_orders.quantity','lz_orders.price','lz_orders.status','lz_orders.note','lz_orders.date','lz_orders.tracking','lz_orders.tracking_url','lz_inventory.ship_code','lz_ship_code.label','master_data.product_name','master_data.main_product_images as image','master_brands.name as brand_name','master_data.product_sku','lz_ship_code.label'))
+					->select(array('lz_orders.quantity','lz_orders.price','lz_orders.status','lz_orders.note','lz_orders.date','lz_orders.tracking','lz_orders.tracking_url', 'master_data.product_name','master_data.main_product_images as image','master_brands.name as brand_name','master_data.product_sku',))
 					->get(); 
 				
 					
