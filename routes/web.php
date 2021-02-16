20ab1eb4bec3baea6a29bf6002521e7312df97e4
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-16 11:24:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -165,7 +165,7 @@ Route::get('/api/order_history', 'API@get_order_history')->middleware(['auth:api
 // Get Order Status
 Route::get('/api/order_status', 'API@get_order_status')->middleware(['auth:api'])->name('get-order-status');
 
-// Save Reported review
+// Save Checkout Email
 Route::post('/api/save_checkout', 'API@save_email_checkout')->middleware(['auth:api']);
 
 // Get Collection Name and SKU count
