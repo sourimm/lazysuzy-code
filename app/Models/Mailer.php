dc732d6482d37576cd9d26b9e77b583f68c3376f
Aditya <adityasaxena602@gmail.com>
2021-02-16 19:48:47 +0530

    add order delivery mailer

--- app/Models/Mailer.php
+++ app/Models/Mailer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -12,19 +12,20 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Models;
 
 
-class Mailer extends Mailable {
-    
+class Mailer extends Mailable
+{
+
     use Queueable, SerializesModels;
 
-    public static function get_mailer_products($user_mail = null) {
+    public static function get_mailer_products($user_mail = null)
+    {
 
         // get_mailer_products will get products from the database
         // for user where E-Mail = $user_mail
 
         if ($user_mail != null) {
             // real logic goes here
-        }
-        else {
+        } else {
             // right now mailer is in testing phase
             // so sending on-sale products of LS_ID = 202 as per @Arzan's directions
 
@@ -39,15 +40,15 @@ class Mailer extends Mailable {
                 ->join("master_brands", "master_data.site_name", "=", "master_brands.value")
                 ->limit($limit)
                 ->get();
-            
+
             $mailer_products = [];
-            foreach($products as $product) {
+            foreach ($products as $product) {
                 $product_details = Product::get_details($product, null, true, false, false, false);
                 // make lazysuzy native product URL
                 $product_details['local_url'] = env('APP_URL') . '/product/' . $product_details['sku'];
                 $mailer_products[] = $product_details;
             }
-            
+
             //return $mailer_products;
 
             /*
@@ -74,71 +75,72 @@ class Mailer extends Mailable {
                 local_url=> "https=>//www.lazysuzy.com/product/PS71108"
             }
             */
-            
+
             echo json_encode($mailer_products);
             //die();
             return $mailer_products;
-            
         }
     }
 
-    public function build() {
+    public function build()
+    {
         $email = "aditya@lazysuzy.com";
         $subject = "Welcome to LazySuzy!";
         $name = "Aditya Saxena";
 
-        return $this->view('pages.productmailer')  
-                    ->from($email, $name)
-                    ->cc($email, $name)
-                    //->replyTo('arzan@lazysuzy.com', $name)
-                    ->subject($subject)
-                    ->with([
-                        'products' => $this->get_mailer_products()
-                    ]);
+        return $this->view('pages.productmailer')
+            ->from($email, $name)
+            ->cc($email, $name)
+            //->replyTo('arzan@lazysuzy.com', $name)
+            ->subject($subject)
+            ->with([
+                'products' => $this->get_mailer_products()
+            ]);
     }
 
-   
-    public static function send_receipt($to, $to_name, $mail_data, $mail_template_id = null) {
+
+    public static function send_receipt($to, $to_name, $mail_data, $mail_template_id = null)
+    {
 
         $mail_template = isset($mail_template_id) ? $mail_template_id : env('MAILER_RECEIPT_TEMPLATE_ID');
- 
+
         $curl = curl_init();
         $subject = str_replace("$", " ", env('MAILER_RECEIPT_SUBJECT'));
         $dy_data = [
-            "personalizations"=> [
+            "personalizations" => [
                 [
-                    "to"=> [
+                    "to" => [
                         [
-                            "email"=> $to,
-                            "name"=> $to_name
+                            "email" => $to,
+                            "name" => $to_name
                         ]
                     ],
                     "dynamic_template_data" => $mail_data,
-                    "subject"=> $to_name . ", " . $subject
+                    "subject" => $to_name . ", " . $subject
                 ]
             ],
-            "from"=> [
-                        "email"=> env('MAILER_FROM'),
-                        "name"=> env('MAILER_FROM_NAME')
-                    ],
-            "reply_to"=> [
-                        "email" => env('MAILER_FROM'),
-                        "name" => env('MAILER_FROM_NAME')
-                    ],
-            "template_id"=> $mail_template
+            "from" => [
+                "email" => env('MAILER_FROM'),
+                "name" => env('MAILER_FROM_NAME')
+            ],
+            "reply_to" => [
+                "email" => env('MAILER_FROM'),
+                "name" => env('MAILER_FROM_NAME')
+            ],
+            "template_id" => $mail_template
         ];
 
-        if(isset($mail_template_id)) {
-            $dy_data['personalizations'][0]['cc'] = [
-                    [
-                        'email' => 'www.lazysuzy.com+d82844d1fe@invite.trustpilot.com'
-                    ]
-                    ];
+        if (isset($mail_template_id)) {
+            $dy_data['personalizations'][0]['bcc'] = [
+                [
+                    'email' => 'www.lazysuzy.com+d82844d1fe@invite.trustpilot.com'
+                ]
+            ];
         }
 
         $auth = [
-                "authorization: Bearer " . env('MAILER_STRIPE_KEY'),
-                "content-type: application/json"
+            "authorization: Bearer " . env('MAILER_STRIPE_KEY'),
+            "content-type: application/json"
         ];
 
         curl_setopt_array($curl, array(
@@ -159,7 +161,7 @@ class Mailer extends Mailable {
 
         curl_close($curl);
 
-        if($httpcode != 202 || $err) {
+        if ($httpcode != 202 || $err) {
             return [
                 'status' => false,
                 'error' => isset($response->errors) ? $response->errors : $err,
