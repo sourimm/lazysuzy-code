ba2db1545c9801429041f46c0cdeba88ce26990a
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-22 22:13:53 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -173,12 +173,12 @@ class PromoDiscount extends Model
 	
 	private static function add_promo_discount($applicable_SKUs, $cart, $promo_details)
     {
-return $applicable_SKUs;
+
         // check if promo is percentage type or flat type
         $promo_type = $promo_details['type'];
         $total_promo_discount = 0;
         foreach ($cart['products'] as &$product) {
-
+return $product;
             // if this SKU is applicable for promo code
             if (in_array($product->product_sku, $applicable_SKUs)) {
                 $total_product_cost_before_discount = (float)$product->total_price;
