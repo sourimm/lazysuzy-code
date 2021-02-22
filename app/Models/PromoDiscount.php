03d51cb0384516277669ee8b99db749141fb3cca
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-22 22:10:09 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -141,7 +141,6 @@ class PromoDiscount extends Model
 	
 	private static function add_promo_discount_for_product($applicable_SKUs, $cart, $promo_details)
     {
-
         // check if promo is percentage type or flat type
         $promo_type = $promo_details['type'];
         $total_promo_discount = 0;
@@ -174,7 +173,7 @@ class PromoDiscount extends Model
 	
 	private static function add_promo_discount($applicable_SKUs, $cart, $promo_details)
     {
-
+return $applicable_SKUs;
         // check if promo is percentage type or flat type
         $promo_type = $promo_details['type'];
         $total_promo_discount = 0;
