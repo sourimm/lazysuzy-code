cb0531dff1852639ef2f15b5c3f40e9792de02ac
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-23 00:20:46 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -58,7 +58,7 @@ class PromoDiscount extends Model
 						foreach($sql as $data){
 							array_push($arr,$data->product_sku);
 						}
-						$cart = self::add_promo_discount($arr, $cart, $promo_details['discount_details']);return $cart;
+						$cart = self::add_promo_discount($arr, $cart, $promo_details['discount_details']);//return $cart;
 					   
 				}
 				
@@ -79,7 +79,7 @@ class PromoDiscount extends Model
 							$promo_apply = $promo_details['discount_details']['apply_on'];
 							
 							if ($promo_apply == Config::get('meta.discount_on_products')) {
-								$cart = self::add_promo_discount($valid_SKUs_for_discount, $cart, $promo_details['discount_details']);return $cart;
+								$cart = self::add_promo_discount($valid_SKUs_for_discount, $cart, $promo_details['discount_details']); 
 							} else {
 								// if promo is to be applied on total order
 								// then we just substract the discount amount from the total_cost 
