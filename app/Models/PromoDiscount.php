685a264746f064e86f61fbd8ed842f0e02d483af
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-02-24 18:24:19 +0530

    Update PromoDiscount.php

--- app/Models/PromoDiscount.php
+++ app/Models/PromoDiscount.php
@@ -166,7 +166,7 @@ class PromoDiscount extends Model
 									if($promo_details['discount_value_json']!=''){
 									  
 										foreach(json_decode($promo_details['discount_value_json']) as $desc_sub){
-											if($product->total_price >= $desc_sub->discount){
+											if($product->total_price >= $desc_sub->price_limit){
 												$promo_discount = $total_product_cost_before_discount * ((float) $desc_sub->discount / 100);
 											}
 											
@@ -182,8 +182,8 @@ class PromoDiscount extends Model
 									$promo_discount = 0;
 									if($promo_details['discount_value_json']!=''){
 									 
-										foreach(json_decode($promo_details['discount_value_json']) as $desc_sub){return 'discount='.$desc_sub->discount;
-											if($product->total_price >= $desc_sub->discount){
+										foreach(json_decode($promo_details['discount_value_json']) as $desc_sub){ 
+											if($product->total_price >= $desc_sub->price_limit){
 												$promo_discount = round((float)$desc_sub->discount, 2); 
 											}
 											
