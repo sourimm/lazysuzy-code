35f8f0d130f7e21d0f83b80cce5ea22e5a79a8da
krishnamzmdr <krishna.mzmdr@gmail.com>
2021-03-01 17:25:42 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -118,7 +118,7 @@ class Product extends Model
     }
 
     public static function get_filter_products($dept, $cat = null, $subCat = null, $isAdmiAPICall = false)
-    {
+    {return 'subcat='.$subCat;
         $perPage = 24;
         $LS_IDs = null;
         $PRICE_ASC = "price_low_to_high";
@@ -285,6 +285,7 @@ class Product extends Model
             $LS_IDs = ['99'];
         }
 
+        if()
         $query = $query->whereRaw('LS_ID REGEXP "' . implode("|", $LS_IDs) . '"');
         $query = DimensionsFilter::apply($query, $all_filters);
         $query = CollectionFilter::apply($query, $all_filters);
